bbdb107f672ee1c007935d7815de0bcf02938c57
Mike J Innes <mike.j.innes@gmail.com>
2017-09-01 23:33:25 -0400

    obsolete

--- mnist/mnist.jl
+++ mnist/mnist.jl
@@ -14,4 +14,4 @@ loss(x, y) = mse(m(x), y)
 train!(loss, repeated((x,y), 1000), SGD(m, 1))
 
 # Check the prediction for the first digit
-onecold(Flux.Tracker.data(m(x[:,1])), 0:9) == onecold(y[:,1], 0:9)
+onecold(m(x[:,1]), 0:9) == onecold(y[:,1], 0:9)
