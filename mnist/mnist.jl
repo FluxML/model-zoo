e668e47a5e464b9087e2a82450c3b361dd099d05
Mike J Innes <mike.j.innes@gmail.com>
2017-09-02 00:00:54 -0400

    add callbacks back

--- mnist/mnist.jl
+++ mnist/mnist.jl
@@ -1,5 +1,5 @@
 using Flux, Flux.Optimise, MNIST, Base.Iterators
-using Flux: onehot, onecold, mse
+using Flux: onehot, onecold, mse, throttle
 
 x, y = traindata()
 y = hcat(map(y -> onehot(y, 0:9), y)...)
@@ -11,7 +11,8 @@ m = Chain(
 
 loss(x, y) = mse(m(x), y)
 
-train!(loss, repeated((x,y), 1000), SGD(m, 1))
+train!(loss, repeated((x,y), 1000), SGD(m, 0.1),
+       cb = throttle(() -> @show(loss(x, y)), 5))
 
 # Check the prediction for the first digit
 onecold(m(x[:,1]), 0:9) == onecold(y[:,1], 0:9)
