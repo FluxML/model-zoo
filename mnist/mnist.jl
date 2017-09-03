7e9fa8ae3faac9b30662cb0d558b8c91b832374f
Mike J Innes <mike.j.innes@gmail.com>
2017-09-03 15:57:25 -0400

    api tweaks

--- mnist/mnist.jl
+++ mnist/mnist.jl
@@ -1,5 +1,6 @@
-using Flux, Flux.Optimise, MNIST, Base.Iterators
-using Flux: onehot, onecold, mse, throttle
+using Flux, MNIST
+using Flux: onehot, onecold, mse, params, throttle
+using Base.Iterators: repeated
 
 x, y = traindata()
 y = hcat(map(y -> onehot(y, 0:9), y)...)
@@ -11,8 +12,8 @@ m = Chain(
 
 loss(x, y) = mse(m(x), y)
 
-train!(loss, repeated((x,y), 1000), SGD(m, 0.1),
-       cb = throttle(() -> @show(loss(x, y)), 5))
+Flux.train!(loss, repeated((x,y), 1000), SGD(params(m), 0.1),
+            cb = throttle(() -> @show(loss(x, y)), 5))
 
 # Check the prediction for the first digit
 onecold(m(x[:,1]), 0:9) == onecold(y[:,1], 0:9)
