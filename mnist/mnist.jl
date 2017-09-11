4ecf81249dc4ccd8c3faf3bacacf2d8bbba1518d
Mike J Innes <mike.j.innes@gmail.com>
2017-09-11 14:12:40 +0100

    rename argmax

--- mnist/mnist.jl
+++ mnist/mnist.jl
@@ -1,5 +1,5 @@
 using Flux, MNIST
-using Flux: onehotbatch, onecold, mse, throttle
+using Flux: onehotbatch, argmax, mse, throttle
 using Base.Iterators: repeated
 
 x, y = traindata()
@@ -16,4 +16,4 @@ Flux.train!(loss, repeated((x,y), 1000), SGD(params(m), 0.1),
             cb = throttle(() -> @show(loss(x, y)), 5))
 
 # Check the prediction for the first digit
-onecold(m(x[:,1]), 0:9) == onecold(y[:,1], 0:9)
+argmax(m(x[:,1]), 0:9) == argmax(y[:,1], 0:9)
