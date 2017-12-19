e01ae73b24270bd1e1203d091c3e9ed364d331ba
Mike J Innes <mike.j.innes@gmail.com>
2017-12-19 12:10:06 +0000

    use flux.cmudict

--- phonemes/0-data.jl
+++ phonemes/0-data.jl
@@ -1,21 +1,19 @@
-# Pkg.clone("https://github.com/FluxML/CMUDict.jl")
-# Pkg.build("CMUDict")
-
-using Flux, CMUDict
+using Flux, Flux.Data.CMUDict
 using Flux: onehot, batchseq
 using Base.Iterators: partition
 
-alphabet = [:end, CMUDict.alphabet...]
-phones = [:start, :end, CMUDict.symbols...]
+dict = cmudict()
+alphabet = [:end, CMUDict.alphabet()...]
+phones = [:start, :end, CMUDict.symbols()...]
 
 tokenise(s, α) = [onehot(c, α) for c in s]
 
 # Turn a word into a sequence of vectors
 tokenise("PHYLOGENY", alphabet)
 # Same for phoneme lists
-tokenise(cmudict["PHYLOGENY"], phones)
+tokenise(dict["PHYLOGENY"], phones)
 
-words = sort(collect(keys(cmudict)), by = length)
+words = sort(collect(keys(dict)), by = length)
 
 # Finally, create iterators for our inputs and outputs.
 batches(xs, p) = [batchseq(b, p) for b in partition(xs, 50)]
@@ -23,10 +21,10 @@ batches(xs, p) = [batchseq(b, p) for b in partition(xs, 50)]
 Xs = batches([tokenise(word, alphabet) for word in words],
              onehot(:end, alphabet))
 
-Ys = batches([tokenise([cmudict[word]..., :end], phones) for word in words],
+Ys = batches([tokenise([dict[word]..., :end], phones) for word in words],
              onehot(:end, phones))
 
-Yo = batches([tokenise([:start, cmudict[word]...], phones) for word in words],
+Yo = batches([tokenise([:start, dict[word]...], phones) for word in words],
              onehot(:end, phones))
 
 data = collect(zip(Xs, Yo, Ys))
