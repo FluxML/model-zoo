ba82d738e0647da1abe598f04a8fad9a314ce437
avik-pal <avikpal@iitk.ac.in>
2018-03-20 20:32:40 +0530

    Made suggested changes
    Removed conv*_* to save space and instead placed the defination of the layer inside Chain itself.

--- cifar10/vgg16.jl
+++ cifar10/vgg16.jl
@@ -16,46 +16,26 @@ labels_test = onehotbatch(test_Y, 0:9)
 train = [(train_x[:,:,:,i], labels[:,i])
          for i in partition(1:50000, 10)]
 
-# Defining the blocks of convolution layers
-
-conv1_1 = Conv((3,3), 3=>64, relu, pad=(1,1), stride=(1,1))
-conv1_2 = Conv((3,3), 64=>64, relu, pad=(1,1), stride=(1,1))
-
-conv2_1 = Conv((3,3), 64=>128, relu, pad=(1,1), stride=(1,1))
-conv2_2 = Conv((3,3), 128=>128, relu, pad=(1,1), stride=(1,1))
-
-conv3_1 = Conv((3,3), 128=>256, relu, pad=(1,1), stride=(1,1))
-conv3_2 = Conv((3,3), 256=>256, relu, pad=(1,1), stride=(1,1))
-conv3_3 = Conv((3,3), 256=>256, relu, pad=(1,1), stride=(1,1))
-
-conv4_1 = Conv((3,3), 256=>512, relu, pad=(1,1), stride=(1,1))
-conv4_2 = Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1))
-conv4_3 = Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1))
-
-conv5_1 = Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1))
-conv5_2 = Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1))
-conv5_3 = Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1))
-
 # Building the model
 
 m = Chain(
-    conv1_1,
-    conv1_2,
+    Conv((3,3), 3=>64, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 64=>64, relu, pad=(1,1), stride=(1,1)),
     x -> maxpool(x, (2,2)),
-    conv2_1,
-    conv2_2,
+    Conv((3,3), 64=>128, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 128=>128, relu, pad=(1,1), stride=(1,1)),
     x -> maxpool(x, (2,2)),
-    conv3_1,
-    conv3_2,
-    conv3_3,
+    Conv((3,3), 128=>256, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 256=>256, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 256=>256, relu, pad=(1,1), stride=(1,1)),
     x -> maxpool(x, (2,2)),
-    conv4_1,
-    conv4_2,
-    conv4_3,
+    Conv((3,3), 256=>512, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1)),
     x -> maxpool(x, (2,2)),
-    conv5_1,
-    conv5_2,
-    conv5_3,
+    Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1)),
+    Conv((3,3), 512=>512, relu, pad=(1,1), stride=(1,1)),
     x -> maxpool(x, (2,2)),
     x -> reshape(x, :, size(x, 4)),
     Dense(512, 4096, relu),
