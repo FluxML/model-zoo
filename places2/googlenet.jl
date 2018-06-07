7ad9eb9eb3374840dfeb75452e76d1ccc2f96e4e
Avik Pal <avikpal@iitk.ac.in>
2018-06-07 11:31:44 +0530

    Fix dimension errors and remove multiple Chains

--- places2/googlenet.jl
+++ places2/googlenet.jl
@@ -13,22 +13,38 @@ end
 
 Flux.treelike(InceptionBlock)
 
-ConvBlock(kernel, chs; stride = (1, 1), pad = (0, 0)) = Chain(Conv(kernel, chs, stride = stride, pad = pad),
-							      BatchNorm(chs[2]),
-							      x -> relu.(x))
+struct ConvBlock
+    convlayer
+    norm
+    nonlinearity
+end
+
+Flux.treelike(ConvBlock)
+
+ConvBlock(kernel, chs; stride = (1, 1), pad = (0, 0)) = ConvBlock(Conv(kernel, chs, stride = stride, pad = pad),
+                                                                  BatchNorm(chs[2]),
+                                                                  x -> relu.(x))
+
+(c::ConvBlock)(x) = c.nonlinearity(c.norm(c.convlayer(x)))
 
 function InceptionBlock(in_chs, chs_1x1, chs_3x3_reduce, chs_3x3, chs_5x5_reduce, chs_5x5, pool_proj)
     path_1 = ConvBlock((1, 1), in_chs=>chs_1x1)
-    path_2 = Chain(ConvBlock((1, 1), in_chs=>chs_3x3_reduce),
-		   ConvBlock((3, 3), chs_3x3_reduce=>chs_3x3, pad = (1, 1)))
-    path_3 = Chain(ConvBlock((1, 1), in_chs=>chs_5x5_reduce),
-		   ConvBlock((5, 5), chs_5x5_reduce=>chs_5x5, pad = (2, 2)))
-    path_4 = Chain(x -> maxpool(x, (3,3), stride = (1, 1), pad = (1, 1)),
-		   ConvBlock((1, 1), in_chs=>pool_proj))
+
+    path_2 = (ConvBlock((1, 1), in_chs=>chs_3x3_reduce),
+              ConvBlock((3, 3), chs_3x3_reduce=>chs_3x3, pad = (1, 1)))
+
+    path_3 = (ConvBlock((1, 1), in_chs=>chs_5x5_reduce),
+              ConvBlock((5, 5), chs_5x5_reduce=>chs_5x5, pad = (2, 2)))
+
+    path_4 = (x -> maxpool(x, (3,3), stride = (1, 1), pad = (1, 1)),
+              ConvBlock((1, 1), in_chs=>pool_proj))
+
     InceptionBlock(path_1, path_2, path_3, path_4)
 end
 
-(m::InceptionBlock)(x) = cat(3, m.path_1(x), m.path_2(x), m.path_3(x), m.path_4(x))
+function (m::InceptionBlock)(x)
+    cat(3, m.path_1(x), m.path_2[2](m.path_2[1](x)), m.path_3[2](m.path_3[1](x)), m.path_4[2](m.path_4[1](x)))
+end
 
 googlenet(num_classes = 365) =
     Chain(ConvBlock((7, 7), 3=>64, stride = (2, 2), pad = (3, 3)),
@@ -43,9 +59,9 @@ googlenet(num_classes = 365) =
       InceptionBlock(512, 160, 112, 224, 24, 64, 64),
       InceptionBlock(512, 128, 128, 256, 24, 64, 64),
       InceptionBlock(512, 112, 144, 288, 32, 64, 64),
-      InceptionBlock(528, 112, 144, 288, 32, 128, 128),
+      InceptionBlock(528, 256, 160, 320, 32, 128, 128),
       x -> maxpool(x, (3, 3), stride = (2, 2), pad = (1, 1)),
-      InceptionBlock(656, 256, 160, 320, 32, 128, 128),
+      InceptionBlock(832, 256, 160, 320, 32, 128, 128),
       InceptionBlock(832, 384, 192, 384, 48, 128, 128),
       x -> meanpool(x, (7, 7), stride = (1, 1), pad = (0, 0)),
       x -> reshape(x, :, size(x, 4)),
--- places2/inceptionresnetv2.jl
+++ places2/inceptionresnetv2.jl
@@ -4,9 +4,19 @@ using Flux: onehotbatch, argmax, @epochs
 using Base.Iterators: partition
 using BSON: @save, @load
 
-function ConvBlock(kernel, chs; stride = (1, 1), pad = (0, 0)) =
-    Chain(Conv(kernel, chs, stride = stride, pad = pad),
-          BatchNorm(chs[2]), x -> relu.(x))
+struct ConvBlock
+    convlayer
+    norm
+    nonlinearity
+end
+
+Flux.treelike(ConvBlock)
+
+ConvBlock(kernel, chs; stride = (1, 1), pad = (0, 0)) = ConvBlock(Conv(kernel, chs, stride = stride, pad = pad),
+                                                                  BatchNorm(chs[2]),
+                                                                  x -> relu.(x))
+
+(c::ConvBlock)(x) = c.nonlinearity(c.norm(c.convlayer(x)))
 
 struct Mixed_5b_7a
     branch1
@@ -20,36 +30,36 @@ Flux.treelike(Mixed_5b_7a)
 function Mixed_5b()
     branch1 = ConvBlock((1, 1), 192=>96)
 
-    branch2 = Chain(ConvBlock((1, 1), 192=>48),
-                    ConvBlock((5, 5), 48=>64, pad = (2, 2)))
+    branch2 = (ConvBlock((1, 1), 192=>48),
+               ConvBlock((5, 5), 48=>64, pad = (2, 2)))
 
-    branch3 = Chain(ConvBlock((1, 1), 192=>64),
-                    ConvBlock((3, 3), 64=>96, pad = (1, 1)),
-                    ConvBlock((3, 3), 96=>96, pad = (1, 1)))
+    branch3 = (ConvBlock((1, 1), 192=>64),
+               ConvBlock((3, 3), 64=>96, pad = (1, 1)),
+               ConvBlock((3, 3), 96=>96, pad = (1, 1)))
 
-    branch4 = Chain(x -> meanpool(x, (3, 3), stride = (1, 1), pad = (1, 1)),
-                    ConvBlock((1, 1), 192=>64, stride = (1, 1)))
+    branch4 = (x -> meanpool(x, (3, 3), stride = (1, 1), pad = (1, 1)),
+               ConvBlock((1, 1), 192=>64, stride = (1, 1)))
 
     Mixed_5b_7a(branch1, branch2, branch3, branch4)
 end
 
 function Mixed_7a()
-    branch1 = Chain(ConvBlock((1, 1), 1088=>256),
-                    ConvBlock((3, 3), 256=>384, stride = (2, 2)))
+    branch1 = x -> maxpool(x, (3, 3), stride = (2, 2))
 
-    branch2 = Chain(ConvBlock((1, 1), 1088=>256),
-                    ConvBlock((3, 3), 256=>288, stride = (2, 2)))
+    branch2 = (ConvBlock((1, 1), 1088=>256),
+              ConvBlock((3, 3), 256=>384, stride = (2, 2)))
 
-    branch3 = Chain(ConvBlock((1, 1), 1088=>256),
-                    ConvBlock((3, 3), 256=>288, pad = (1, 1)),
-                    ConvBlock((3, 3), 288=>320, stride = (2, 2)))
+    branch3 = (ConvBlock((1, 1), 1088=>256),
+              ConvBlock((3, 3), 256=>288, pad = (1, 1)),
+              ConvBlock((3, 3), 288=>320, stride = (2, 2)))
 
-    branch4 = x -> maxpool(x, (3, 3), stride = (2, 2))
+    branch4 = (ConvBlock((1, 1), 1088=>256),
+              ConvBlock((3, 3), 256=>288, stride = (2, 2)))
 
     Mixed_5b_7a(branch1, branch2, branch3, branch4)
 end
 
-(m::Mixed_5b_7a)(x) = cat(3, m.branch1(x), m.branch2(x), m.branch3(x), m.branch4(x))
+(m::Mixed_5b)(x) = cat(3, m.branch1(x), m.branch2[2](m.branch2[1](x)), m.branch3[3](m.branch3[2](m.branch3[1](x))), m.branch4[2](m.branch4[1](x)))
 
 struct Block35
     scale
@@ -64,19 +74,19 @@ Flux.treelike(Block35)
 function Block35(scale = 1.0f0)
     branch1 = ConvBlock((1, 1), 320=>32)
 
-    branch2 = Chain(ConvBlock((1, 1), 320=>32),
-                    ConvBlock((3, 3), 32=>32, pad = (1, 1)))
+    branch2 = (ConvBlock((1, 1), 320=>32),
+               ConvBlock((3, 3), 32=>32, pad = (1, 1)))
 
-    branch3 = Chain(ConvBlock((1, 1), 320=>32),
-                    ConvBlock((3, 3), 32=>48, pad = (1, 1)),
-                    ConvBlock((3, 3), 48=>64, pad = (1, 1)))
+    branch3 = (ConvBlock((1, 1), 320=>32),
+               ConvBlock((3, 3), 32=>48, pad = (1, 1)),
+               ConvBlock((3, 3), 48=>64, pad = (1, 1)))
 
     convlayer = Conv((1, 1), 128=>320)
 
     Block35(scale, branch1, branch2, branch3, convlayer)
 end
 
-(b::Block35)(x) = relu.(b.convlayer(cat(3, b.branch1(x), b.branch2(x), b.branch3(x))) * b.scale + x)
+(b::Block35)(x) = relu.(b.convlayer(cat(3, b.branch1(x), b.branch2[2](b.branch2[1](x)), b.branch3[3](b.branch3[2](b.branch3[1](x))))) * b.scale + x)
 
 struct Mixed_6a
     branch1
@@ -89,16 +99,16 @@ Flux.treelike(Mixed_6a)
 function Mixed_6a()
     branch1 = ConvBlock((3, 3), 320=>384, stride = (2, 2))
 
-    branch2 = Chain(ConvBlock((1, 1), 320=>256),
-                    ConvBlock((3, 3), 256=>256, pad = (1, 1)),
-                    ConvBlock((3, 3), 256=>384, stride = (2, 2)))
+    branch2 = (ConvBlock((1, 1), 320=>256),
+               ConvBlock((3, 3), 256=>256, pad = (1, 1)),
+               ConvBlock((3, 3), 256=>384, stride = (2, 2)))
 
     branch3 = x -> maxpool(x, (3, 3), stride = (2, 2))
 
     Mixed_6a(branch1, branch2, branch3)
 end
 
-(m::Mixed_6a)(x) = cat(3, m.branch1(x), m.branch2(x), m.branch3(x))
+(m::Mixed_6a)(x) = cat(3, m.branch1(x), m.branch2[3](m.branch2[2](m.branch2[1](x))), m.branch3(x))
 
 struct Block17
     scale
@@ -112,16 +122,16 @@ Flux.treelike(Block17)
 function Block17(scale = 1.0f0)
     branch1 = ConvBlock((1, 1), 1088=>192)
 
-    branch2 = Chain(ConvBlock((1, 1), 1088=>128),
-                    ConvBlock((1, 7), 128=>160, pad = (0, 3)),
-                    ConvBlock((7, 1), 160=>192, pad = (3, 0)))
+    branch2 = (ConvBlock((1, 1), 1088=>128),
+               ConvBlock((1, 7), 128=>160, pad = (0, 3)),
+               ConvBlock((7, 1), 160=>192, pad = (3, 0)))
 
     convlayer = Conv((1, 1), 384=>1088)
 
     Block17(scale, branch1, branch2, convlayer)
 end
 
-(b::Block17)(x) = relu.(b.convlayer(cat(3, b.branch1(x), b.branch2(x))) * b.scale + x)
+(b::Block17)(x) = relu.(b.convlayer(cat(3, b.branch1(x), b.branch2[3](b.branch2[2](b.branch2[1](x))))) * b.scale + x)
 
 struct Block8
     scale
@@ -136,9 +146,9 @@ Flux.treelike(Block8)
 function Block8(scale = 1.0f0, norelu = false)
     branch1 = ConvBlock((1, 1), 2080=>192)
 
-    branch2 = Chain(ConvBlock((1, 1), 2080=>192),
-                    ConvBlock((1, 3), 192=>224, pad = (0, 1)),
-                    ConvBlock((3, 1), 224=>256, pad = (1, 0)))
+    branch2 = (ConvBlock((1, 1), 2080=>192),
+               ConvBlock((1, 3), 192=>224, pad = (0, 1)),
+               ConvBlock((3, 1), 224=>256, pad = (1, 0)))
 
     convlayer = Conv((1, 1), 448=>2080)
 
@@ -146,7 +156,7 @@ function Block8(scale = 1.0f0, norelu = false)
 end
 
 function (b::Block8)(x)
-    ẋ = b.convlayer(cat(3, b.branch1(x), b.branch2(x))) * b.scale + x
+    ẋ = b.convlayer(cat(3, b.branch1(x), b.branch2[3](b.branch2[2](b.branch3[1](x))))) * b.scale + x
     if(!b.norelu)
         ẋ = relu.(ẋ)
     end
