f9e36c89888943afc67ab7318f7c8be1ffa9b3f9
Tejan Karmali <tejank10@gmail.com>
2018-07-22 12:59:29 -0400

    working DNI MLP model

--- vision/mnist/dni_mlp.jl
+++ vision/mnist/dni_mlp.jl
@@ -1,7 +1,7 @@
-using Flux, Flux.Tracker
-using Flux: onehotbatch, argmax, crossentropy, throttle
-using Base.Iterators: repeated
-using CuArrays, CUDAnative
+using Flux, Flux.Data.MNIST
+using Flux: onehotbatch, argmax, crossentropy
+using CuArrays
+
 ################################################################################
 # Decoupled Neural Interface consists of two parts: A feedforward neural network
 # and a neural network to compute gradients. `net` is the FFNN here and dni is
@@ -11,7 +11,8 @@ struct SynthMLP
   net::Chain
   dni::Chain
   conditioned::Bool
-  opt
+  opt_net
+  opt_dni
 end
 
 
@@ -20,94 +21,118 @@ function SynthMLP(input_dim::Int, output_dim::Int, hidden_dims = [];
 
   forward = []
   forward_dni = []
+  opt_net, opt_dni = [], []
   dims = push!(append!([input_dim], hidden_dims), output_dim)
-
+  
   for i = 1:length(dims)-1
-    push!(forward, Dense(dims[i], dims[i+1], act_fns[i]))
+    push!(forward, Dense(dims[i], dims[i+1], act_fns[i]))   
     d = dims[i+1] + output_dim * conditioned
-    push!(forward_dni, Dense(d, dims[i+1]))
+    push!(forward_dni, Dense(d, dims[i+1], initW = zeros))
   end
 
   net = Chain(forward...) |> gpu
-  dni = Chain(forward_dni...) |> gpu
+  # Last layer does not require synthetic grads, it can directly obtain true grads
+  dni = Chain(forward_dni[1:end-1]...) |> gpu
 
-  SynthMLP(net, dni, conditioned, ADAM(vcat(params(net),  params(dni))))
+  for (layer, dni_layer) in zip(net.layers[1:end-1], dni.layers)
+    push!(opt_net, ADAM(params(layer)))
+    push!(opt_dni, ADAM(params(dni_layer)))
+  end
+  
+  push!(opt_net, ADAM(params(net.layers[end])))
+
+  SynthMLP(net, dni, conditioned, opt_net, opt_dni)
 end
 
 (s_mlp::SynthMLP)(x) = s_mlp.net(x)
+
 #################### Training a layer using Synthetic Gradients ################
-function train_layer(layer, dni_layer, x, y = nothing; conditioned=false)
+
+function train_layer(layer, dni_layer, opt_layer, x, y = nothing; conditioned=false)
   inp = param(x)
   out = layer(inp)
 
   # Concatenate labels to DNI input if DNI is conditioned on labels
   if conditioned
     @assert size(y, 2) == size(out, 2)
-    dni_inp = vcat(out, param(y))
+    dni_inp = vcat(out.data, y)
   else
-    dni_inp = out
+    dni_inp = out.data
   end
   # This is predicted gradient of `out`; ie dLoss / dout
   grad = dni_layer(dni_inp)
-  # Backpropagating the prediected grads to the Weights and input
-  Flux.back!(out, grad.data)
+  # Backpropagating the predicted grads to the Weights and input
+  Flux.back!(out, grad.data)  
+  
+  opt_layer()
+  
   #returning the output, ∇̂output, ∇input
   out.data, grad, inp.grad
 end
 
-function train_net(s_mlp::SynthMLP, x, y = nothing; loss = (a,b)->crossentropy(softmax(a),b),
+# Last layer does not use any synthetic gradients
+function train_last_layer(layer, opt_layer, x, y, loss)
+  inp = param(x)
+  out = layer(inp)
+
+  l = loss(out, y)
+  Flux.back!(l)
+
+  opt_layer()
+
+  out.data, inp.grad, l.data
+end
+
+function train_net(s_mlp::SynthMLP, x, y = nothing; loss = (a,b)->crossentropy(softmax(a), b),
   synthLoss = (a,b)->Flux.mse(a,b))
   out = x
   dĥ, _dh = [], [] # predicted grads of output of a layer
                    # true grads of input to a layer
 
-  for (layer, dni_layer) in zip(s_mlp.net.layers, s_mlp.dni.layers)
-    out, Δ, _Δ = train_layer(layer, dni_layer, out, y; conditioned = s_mlp.conditioned)
+  for (layer, dni_layer, opt_layer) in zip(s_mlp.net.layers[1:end-1], s_mlp.dni.layers, s_mlp.opt_net[1:end-1])
+    out, Δ, _Δ = train_layer(layer, dni_layer, opt_layer, out, y; conditioned = s_mlp.conditioned)				    
     push!(dĥ, Δ)
     push!(_dh, _Δ)
   end
-
-  # Taking true gradient of final output
-  cc, back = Tracker.forward((a, b)->loss(a, b), out, y)
-
-  dout, _ = Tracker.data.(back(ones(out))) |> gpu
+  
+  out, _Δ, loss_ = train_last_layer(s_mlp.net.layers[end], s_mlp.opt_net[end], out, y, loss)
 
   # Gradient of the input data to the network is not required
-  dh = push!(_dh[2:end], dout)
-  for (dŷ, dy) in zip(dĥ, dh)
-    # Taking Synthetic loss for training the DNI
-    @show l = synthLoss(dŷ, dy)
-    Flux.back!(l)
-  end
-
-  # optimising the parameters using accumulated grads
-  s_mlp.opt()
+  dh = push!(_dh[2:end], _Δ)
   
-  loss(out, y)
+  for (dŷ, dy, opt_dni_layer) in zip(reverse.([dĥ, dh, s_mlp.opt_dni])...)
+    # Taking Synthetic loss for training the DNI
+    l = synthLoss(dŷ, dy)
+    Flux.back!(l)  
+    opt_dni_layer()
+  end
+  
+  loss_
 end
 
 ################################################################################
 
-using Flux.Data.MNIST
-
 imgs = MNIST.images()
+
 # Stack images into one large batch
 X = hcat(float.(reshape.(imgs, :))...) |> gpu
+Y = float.(onehotbatch(MNIST.labels(), 0:9)) |> gpu
 
-labels = MNIST.labels()
-# One-hot-encode the labels
-Y = Float32.(onehotbatch(labels, 0:9)) |> gpu
-
-dataset = repeated((X, Y), 200)
-
-model = SynthMLP(784, 10, [256], act_fns = [relu, identity])
+model = SynthMLP(784, 10, [256]; act_fns = [relu, identity])
 
 accuracy(x, y) = mean(argmax(model(x)) .== argmax(y))
 
-epochs = 1
-for (x, y) in dataset
-  l = train_net(model, x, y)
-  acc = accuracy(x, y)
-  println("Epoch $epochs over. Loss: $l. Accuracy: $acc")
-  epochs += 1
+for epoch = 1:200 
+  l   = train_net(model, X, Y)
+  acc = accuracy(X, Y)
+  println("Epoch: $epoch. Loss: $l. Accuracy: $acc")
 end
+
+
+# Let's check the accuracy on testset
+
+tX = hcat(float.(reshape.(MNIST.images(:test), :))...) |> gpu
+tY = onehotbatch(MNIST.labels(:test), 0:9) |> gpu
+
+acc = accuracy(tX, tY)
+println("Accuracy on testset: $acc")
