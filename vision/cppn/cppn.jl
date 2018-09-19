f90d818cb8ff9c227e3fd0d91c4efdb1ce77aa78
Roboneet <neethumariyajoy@gmail.com>
2018-09-20 04:27:54 +0530

    update

--- vision/cppn/cppn.jl
+++ vision/cppn/cppn.jl
@@ -1,44 +1,59 @@
+# Ref: http://blog.otoro.net/2016/03/25/generating-abstract-patterns-with-tensorflow/
+
 using Images
 using Flux
 
+# set parameters
 z_dim = 2
-x_dim = 264
-y_dim = 264
-net_size = 15
-net_depth = 5
-batch_size = 512
+x_dim = 512
+y_dim = 512
+N = 14
+hidden = 9
+batch_size = 1024
 n = x_dim * y_dim
 
-cast(x) = (collect(0:x-1) ./ x) .- 0.5
+# cast 0:x-1 to -0.5:0.5
+cast(x) = [range(-0.5, stop=0.5, step=1/(x - 1))...]
 
 xs, ys = cast(x_dim), cast(y_dim)
 xs = repeat(xs, inner=(y_dim))
-ys = repeat(ys, outer=(y_dim))
+ys = repeat(ys, outer=(x_dim))
 rs = sqrt.(xs.^2 + ys.^2)
 
-layers = []
-push!(layers, Dense(3 + z_dim, net_size, tanh, initW=randn))
-for i=1:net_depth
-    push!(layers, Dense(net_size, net_size, tanh, initW=randn))
+# sample weigths from a gaussian distribution
+unit(in=N, out=N, f=tanh) = Dense(in, out, f, initW=randn)
+
+# input -> [x, y, r, z...]
+layers = Any[unit(3 + z_dim)]
+for i=1:hidden
+    push!(layers, unit())
 end
-push!(layers, Dense(net_size, 1, σ, initW=randn))
+push!(layers, unit(N, 1, σ))
 
 model = Chain(layers...)
+getColorAt(x) = Flux.data(model(x))
 
-getColorAt(x) = model(x).data
+function batch(arr, s)
+    batches = []
+    l = size(arr, 2)
+    for i=1:s:l
+        push!(batches, arr[:, i:min(i+s-1, l)])
+    end
+    batches
+end
 
 function getImage(z)
     z = repeat(reshape(z, 1, z_dim), outer=(n, 1))
     coords = hcat(xs, ys, rs, z)'
-    coords = [coords[:, i:min(i + batch_size - 1, size(coords, 2))]
-        for i=1:batch_size:size(coords, 2)]
-    hcat(getColorAt.(coords)...)
+    coords = batch(coords, batch_size)
+    pixels = [Gray.(hcat(getColorAt.(coords)...))...]
+    reshape(pixels, y_dim, x_dim)
 end
 
-function showImg(z, image_path="sample.png")
-    imgg = Gray.(reshape([getImage(z)...], y_dim, x_dim))
+function saveImg(z, image_path="sample.png")
+    imgg = getImage(z)
     save(image_path, imgg)
     imgg
 end
 
-showImg(rand(z_dim))
+saveImg(rand(z_dim))
