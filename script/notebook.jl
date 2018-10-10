052764a28db2466b94963008a0135a1e7d178638
Dhairya Gandhi <dhairya@juliacopmuting.com>
2018-10-10 10:13:05 +0530

    added control for particular projects

--- script/notebook.jl
+++ script/notebook.jl
@@ -1,6 +1,9 @@
 using Pkg.TOML
 meta = TOML.parsefile(joinpath(@__DIR__, "Notebooks.toml"))
 
-for proj in keys(meta)
-  run(`julia1 convert.jl $proj`)
+length(ARGS) > 0 && (meta = ARGS)
+meta isa Dict && (meta = keys(meta))
+
+for proj in meta
+  run(`julia convert.jl $proj`)
 end
