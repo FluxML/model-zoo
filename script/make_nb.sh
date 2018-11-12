71d7126bedbf79516eaf570e5b02cd23a8466373
Dhairya Gandhi <dhairya@juliacopmuting.com>
2018-11-12 20:18:32 +0530

    fix remote connection

--- script/make_nb.sh
+++ script/make_nb.sh
@@ -1,3 +1,3 @@
-scp trial.sh dhairyagandhi96@gpublr.juliacomputing.io:~/
-ssh dhairyagandhi96@gpublr.juliacomputing.io "chmod +x ~/driver.sh"
-ssh -o ConnectTimeout=10 dhairyagandhi96@gpublr.juliacomputing.io "~/driver.sh"
+scp trial.sh <user>@ip:~/
+ssh <user>@ip "chmod +x ~/driver.sh"
+ssh -o ConnectTimeout=10 <user>@ip "~/driver.sh"
