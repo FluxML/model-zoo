91bf1e4726eecfa5606e9700dcc6fe51a3eb1711
Dhairya Gandhi <dhairya@juliacopmuting.com>
2018-12-15 14:56:14 +0530

    extended blitz

--- tutorials/60-minute-blitz.jl
+++ tutorials/60-minute-blitz.jl
@@ -211,10 +211,153 @@ params(m)
 
 m = Chain(Dense(10, 5, relu), Dense(5, 2), softmax)
 
-l = sum(Flux.crossentropy(m(x), [0.5, 0.5]))
+loss(x, y = [0.5, 0.5]) = sum(Flux.crossentropy(m(x), y))
+l = loss(x)
 back!(l)
 
 grad.(params(m))
 
 # You don't have to use layers, but they can be convient for many simple kinds
-# of models.
+# of models and fast iteration.
+
+# The next step is to update our weights and perform optimisation. As you might be
+# familiar, *Gradient Descent* is a simple algorithm that takes the weights and steps
+# using a learning rate and the gradients. `weights = weights - learning_rate * gradient`.
+
+using Flux.Tracker: update!
+
+η = 0.1
+for p in params(m)
+  update!(p, -η * grad(p))
+end
+
+# While this is a valid way of updating our weights, it can get more complicated as the
+# algorithms we use get more involved. 
+
+# Flux comes with a bunch of pre-defined optimisers and makes writing our own really simple.
+# We just give it the model parameters and any other parameters of the optimisers themselves.
+
+opt = SGD(params(m), 0.01)
+opt() # updates the weights
+
+# Combining all these steps put together, done many times is how we train Flux models.
+# Flux comes with the `train!` to start our training loop.
+
+data, labels = rand(10, 100), fill(0.5, 2, 100)
+Flux.train!(loss, [(data, labels)], opt)
+
+# You don't have to use `train!`. In cases where aribtrary logic might be better suited,
+# you could open up this training loop like so:
+
+# ```julia
+#   for d in training_set # assuming d looks like (data, labels)
+#     # our super logic
+#     l = loss(d...)
+#     Tracker.back!(l)
+#     opt()
+#   end
+# ```
+
+# Training a Classifier
+# ---------------------
+
+# Getting a real classifier to work might help cement the workflow a bit more.
+# [CIFAR10](url) is a dataset of 50k tiny training images split into 10 classes.
+
+# We will do the following steps in order:
+
+# * Load CIFAR10 training and test datasets
+# * Define a Convolution Neural Network
+# * Define a loss function
+# * Train the network on the training data
+# * Test the network on the test data
+
+# Loading the Dataset
+
+# [Metalhead.jl](https://github.com/FluxML/Metalhead.jl) is an excellent package
+# that has a number of predefined and pretrained computer vision models.
+# It also has a number of dataloaders that come in handy to load datasets.
+
+using Statistics
+using Flux, Flux.Tracker, Flux.Optimise
+using Metalhead, Images
+using Metalhead: trainimgs
+using Images.ImageCore
+using Flux: onehotbatch, onecold
+using Base.Iterators: partition
+using CuArrays
+
+# The image will give us an idea of what we are dealing with.
+# ![title](https://pytorch.org/tutorials/_images/cifar10.png)
+
+Metalhead.download(CIFAR10);
+X = trainimgs(CIFAR10);
+labels = onehotbatch([X[i].ground_truth.class for i in 1:50000],1:10);
+
+# Let's take a look at a random image from the dataset
+
+X[rand(1:end)].img;
+
+# The images are simply 32 X 32 matrices of numbers in 3 channels (R,G,B). We can now
+# arrange them in batches of say, 1000 and keep a validation set to track our progress.
+
+# Defining a `getarray` function would help in converting the matrices to `Float` type.
+
+getarray(X) = float.(permutedims(channelview(X), (2, 3, 1)));
+imgs = [getarray(X[i].img) for i in 1:50000];
+
+# The first 49k images (in batches of 1000) will be our training set, and the rest is
+# for validation.
+
+train = gpu.([(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(1:49000, 1000)]);
+valset = 49001:50000
+valX = cat(imgs[valset]..., dims = 4) |> gpu;
+valY = labels[:, valset] |> gpu;
+
+# Now we can define our Convolutional Neural Network (CNN).
+
+m = Chain(
+  Conv((5,5), 3=>16, relu),
+  x -> maxpool(x, (2,2)),
+  Conv((5,5), 16=>8, relu),
+  x -> maxpool(x, (2,2)),
+  x -> reshape(x, :, size(x, 4)),
+  Dense(200, 120),
+  Dense(120, 84),
+  Dense(84, 10),
+  softmax) |> gpu
+
+# We will use a crossentropy loss and an Momentum optimiser here.
+
+using Flux: crossentropy, Momentum
+
+loss(x, y) = sum(crossentropy(m(x), y))
+opt = Momentum(params(m))
+
+# We can start writing our train loop where we will keep track of some basic accuracy
+# numbers about our model. We can define an `accuracy` function for it like so.
+
+accuracy(x, y) = mean(onecold(m(x), 1:10) .== onecold(y, 1:10))
+
+epochs = 100
+
+for epoch = 1:epochs
+  for d in train
+    l = loss(d...)
+    back!(l)
+    opt()
+    @show accuracy(valX, valY)
+  end
+end
+
+# The `gpu` functions you see sprinkled through this bit of the code tell Flux to move
+# these entities to GPU if available, and subsequently train on them. No extra faffing
+# about required! The same bit of code would work on any hardware with some small
+# annotations like you saw here.
+
+# Let's see how the model fared.
+
+accuracy(valX, valY)
+
+# This is much better than random chance, and not bad at all for a small hand written
+# network like ours.
