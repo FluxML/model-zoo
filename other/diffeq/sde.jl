eeb430f371ed4a4d8cbb06a207d3857eeb36b5bb
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-01-24 10:52:47 -0500

    fix DDE/SDE naming

--- other/diffeq/sde.jl
+++ other/diffeq/sde.jl
@@ -1,30 +1,28 @@
 using Flux, DiffEqFlux, DifferentialEquations, Plots
 
-## Setup DDE to optimize
-function delay_lotka_volterra(du,u,h,p,t)
+function lotka_volterra(du,u,p,t)
   x, y = u
   α, β, δ, γ = p
-  du[1] = dx = (α - β*y)*h(p,t-0.1)[1]
-  du[2] = dy = (δ*x - γ)*y
+  du[1] = dx = α*x - β*x*y
+  du[2] = dy = -δ*y + γ*x*y
 end
-
-h(p,t) = ones(eltype(p),2)
-prob = DDEProblem(delay_lotka_volterra,[1.0,1.0],h,(0.0,10.0),constant_lags=[0.1])
+function lotka_volterra_noise(du,u,p,t)
+  du[1] = 0.1u[1]
+  du[2] = 0.1u[2]
+end
+prob = SDEProblem(lotka_volterra,lotka_volterra_noise,[1.0,1.0],(0.0,10.0))
 p = param([2.2, 1.0, 2.0, 0.4])
-function predict_rd_dde()
-  diffeq_rd(p,prob,MethodOfSteps(Tsit5()),saveat=0.1)[1,:]
+function predict_fd_sde()
+  diffeq_fd(p,sol->sol[1,:],101,prob,SOSRI(),saveat=0.1)
 end
-loss_rd_dde() = sum(abs2,x-1 for x in predict_rd_dde())
-
-# Optimize the parameters so the DDE's solution stays near 1
+loss_fd_sde() = sum(abs2,x-1 for x in predict_fd_sde())
 
 data = Iterators.repeated((), 100)
 opt = ADAM(0.1)
-cb = function () #callback function to observe training
-  display(loss_rd_dde())
-  # using `remake` to re-create our `prob` with current parameters `p`
-  display(plot(solve(remake(prob,p=Flux.data(p)),MethodOfSteps(Tsit5()),saveat=0.1),ylim=(0,6)))
+cb = function ()
+  display(loss_fd_sde())
+  display(plot(solve(remake(prob,p=Flux.data(p)),SOSRI(),saveat=0.1),ylim=(0,6)))
 end
-# Display the DDE with the initial parameter values.
+# Display the SDE with the current parameter values.
 cb()
-Flux.train!(loss_rd_dde, [p], data, opt, cb = cb)
+Flux.train!(loss_fd_sde, [p], data, opt, cb = cb)
