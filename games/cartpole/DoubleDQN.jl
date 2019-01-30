2ec88359546d770604755813e6705907eec2146e
Sidhant Nagpal <sidhantnagpal97@gmail.com>
2019-01-31 00:25:07 +0530

    Add DoubleDQN Implementation for CartPoleV0

--- games/cartpole/DoubleDQN.jl
+++ games/cartpole/DoubleDQN.jl
@@ -12,7 +12,7 @@ env = CartPoleV0()
 
 
 #-------------------------Parameters-----------------------#
-EPISODES = 500
+EPISODES = 400
 STATE_SIZE = length(env.state)
 ACTION_SIZE = length(actions(env, env.state))
 REPLAY_MEMORY = 10000
@@ -23,22 +23,24 @@ BATCH_SIZE = 32
 γ = 0.99                # discount rate
 η = 0.0001              # learning rate
 
-ϵ = 0.9                 # exploration rate
+ϵ = 1.0                 # exploration rate
 ϵ_min = 0.01            # exploration minimum
-ϵ_decay = 0.995         # exploration decay
+ϵ_decay = 0.99          # exploration decay
 
 memory = CircularBuffer{Any}(REPLAY_MEMORY)
 
 
 #-----------------------Model Architecture------------------------#
 model = Chain(Dense(STATE_SIZE, 24, relu),
-              Dense(24, 48, relu), 
-              Dense(48, ACTION_SIZE)) |> gpu
+              Dense(24, 24, relu), 
+              Dense(24, ACTION_SIZE)) |> gpu
 
 loss(x, y) = Flux.mse(model(x), y)
 opt = ADAM(η)
 
 fit_model(dataset) = Flux.train!(loss, params(model), dataset, opt)
+
+model_target = deepcopy(model)
 
 """Save sample (s, a, r, s′) to replay memory"""
 function remember(state, action, reward, next_state, done)
@@ -53,7 +55,6 @@ function act(state, ϵ)
     return argmax(q_values)
 end
 
-
 """Sample from replay memory, train model, update exploration"""
 function replay()
     length(memory) < BATCH_SIZE && return nothing
@@ -66,17 +67,21 @@ function replay()
     s′b = hcat(s′b...) |> gpu
     
     qb_target = model(sb).data
-    qb_learned = maximum(model(s′b).data, dims=1)
+    
+    actions = argmax(model(s′b).data)
+    qb_learn = model_target(s′b).data
+    qb_learned = getindex.(Ref(qb_learn), actions)
+    
     qb_learned = ifelse.(db, rb, rb .+ γ .* cpu(qb_learned))
     setindex!.(Ref(qb_target), qb_learned, ab)
     
     dataset = [(sb, qb_target)]
     fit_model(dataset)
     
-    global ϵ
-    ϵ > ϵ_min && (ϵ *= ϵ_decay)
-    
     GC.gc(); # CuArrays.clearpool()
+    
+    global ϵ
+    ϵ = ϵ_min + (ϵ - ϵ_min)*ϵ_decay
 end
 
 #----------------------------Training & Testing---------------------------#
@@ -109,16 +114,17 @@ for e=1:EPISODES
     if best_score <= score
         best_score = score
         println(stats); flush(stdout)
-        @save "models/dqn/model-$e-$score.bson" model
+        @save "models/notebook4/model-$e-$score.bson" model
         anim = @animate for env in envs
             plot(env)
         end
-        mp4(anim, "models/dqn/env-$e-$score.mp4", fps=20, show_msg=false)
+        mp4(anim, "models/notebook4/env-$e-$score.mp4", fps=20, show_msg=false)
     else
         print(stats); flush(stdout); print("\r")
     end
     
     replay()
+    model_target = deepcopy(model)
     
     if e % test_every == 0
         score = 0
@@ -130,7 +136,7 @@ for e=1:EPISODES
                 action = act(state, ϵ_min)
                 reward, state = step!(env, state, action)
                 done = finished(env, state)
-                reward = !done ? reward : -1
+                reward = !done ? reward : -1 
                 score += reward
 
                 done && break
