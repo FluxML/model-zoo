c0f852c54dcf02ec0d5fc1317e366e3e14ad68b5
thebhatman <manjunathbhat9920@gmail.com>
2019-02-05 01:08:36 +0530

    Training set randomized to avoid bias, accuracy increased

--- other/fizzbuzz/fizzbuzz.jl
+++ other/fizzbuzz/fizzbuzz.jl
@@ -2,6 +2,8 @@
 # http://joelgrus.com/2016/05/23/fizz-buzz-in-tensorflow/
 using Flux: Chain, Dense, params, crossentropy, onehotbatch,
             ADAM, train!, softmax
+
+using Random
 using Test
 
 # Data preparation
@@ -22,25 +24,25 @@ end
 
 const LABELS = ["fizz", "buzz", "fizzbuzz", "else"];
 
-@test fizzbuzz.([3, 5, 15, 98]) == LABELS
 
-raw_x = 1:100;
+
+raw_x = 100:10000; #Made training dataset larger and exclusive of test set
 raw_y = fizzbuzz.(raw_x);
 
 # Feature engineering
-features(x) = float.([x % 3, x % 5, x % 15])
+features(x) = float.([x%3 , x%5 , x%15 ])
 features(x::AbstractArray) = hcat(features.(x)...)
 
 X = features(raw_x);
 y = onehotbatch(raw_y, LABELS);
 
 # Model
-m = Chain(Dense(3, 10), Dense(10, 4), softmax)
+m = Chain(Dense(3, 100), Dense(100, 4), softmax)
 loss(x, y) = crossentropy(m(X), y)
 opt = ADAM()
 
 # Helpers
-deepbuzz(x) = (a = argmax(m(features(x))); a == 4 ? x : LABELS[a])
+deepbuzz(x) = (a = argmax(m(features(x))); a = LABELS[a])
 
 function monitor(e)
     print("epoch $(lpad(e, 4)): loss = $(round(loss(X,y).data; digits=4)) | ")
@@ -49,6 +51,16 @@ end
 
 # Training
 for e in 0:1000
+    #Randomizing training data to avoid sequential bias
+    Random.shuffle!(float.(raw_x))
+    raw_y = fizzbuzz.(raw_x)
+    X = features(raw_x);
+    y = onehotbatch(raw_y, LABELS);
     train!(loss, params(m), [(X, y)], opt)
     if e % 50 == 0; monitor(e) end
 end
+#Test Cases added
+@test fizzbuzz.([3, 5, 15, 98]) == deepbuzz.([3,5,15,98])
+@test fizzbuzz.([10,9,60, 99]) == deepbuzz.([10,9,60,99])
+@test fizzbuzz.([35, 42, 99, 3]) == deepbuzz.([35,42,99,3])
+@test fizzbuzz.([55,64,72,18]) == deepbuzz.([55,64,72,18])
