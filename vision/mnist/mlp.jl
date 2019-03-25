cf3da4fb4967ff151478f884f8037d122e87e747
shashikdm <shashikant.123kadam@gmail.com>
2019-03-25 22:31:58 +0530

    Correction

--- vision/mnist/mlp.jl
+++ vision/mnist/mlp.jl
@@ -1,11 +1,8 @@
 using Flux, Flux.Data.MNIST, Statistics
 using Flux: onehotbatch, onecold, crossentropy, throttle
 using Base.Iterators: repeated
-using JLD2, FileIO
-
-@load "temp.jld2" temp
 # using CuArrays
-checkthis = temp;
+
 # Classify MNIST digits with a simple multi-layer-perceptron
 
 imgs = MNIST.images()
@@ -15,10 +12,9 @@ X = hcat(float.(reshape.(imgs, :))...) |> gpu
 labels = MNIST.labels()
 # One-hot-encode the labels
 Y = onehotbatch(labels, 0:9) |> gpu
+
 m = Chain(
-  Dense(784,32),
-  Dense(32, 32),
-  x -> Tracker.data(x),
+  Dense(28^2, 32, relu),
   Dense(32, 10),
   softmax) |> gpu
 
@@ -32,16 +28,10 @@ opt = ADAM()
 
 Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10))
 
-println(accuracy(X, Y))
+accuracy(X, Y)
 
 # Test set accuracy
 tX = hcat(float.(reshape.(MNIST.images(:test), :))...) |> gpu
 tY = onehotbatch(MNIST.labels(:test), 0:9) |> gpu
 
-println(accuracy(tX, tY))
-if checkthis == m[1]
-	println("Yes")
-else
-	println("No")
-end
-
+accuracy(tX, tY)
