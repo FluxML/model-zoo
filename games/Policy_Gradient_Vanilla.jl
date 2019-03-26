e937fa5aff3aac35ba3be58ea79cd61e0f2e3fa4
Shreyas <shreyaskowshik@gmail.com>
2019-03-27 00:14:40 +0530

    Made Requested Changes

--- games/Policy_Gradient_Vanilla.jl
+++ games/Policy_Gradient_Vanilla.jl
@@ -15,32 +15,52 @@ mutable struct CartPolePolicy <: Reinforce.AbstractPolicy
   end
 end
 
-#----------Setup Environment-------
+# #----------Setup Environment-------
 env = GymEnv("CartPole-v0")
 
-#----------Define Hyperparameters-------
+# #----------Define Hyperparameters-------
 STATE_SIZE = length(env.state)
 ACTION_SIZE = length(env.actions)
-γ = 1.00f0    # discount rate
+γ = 1.0f0    # discount rate
 
 η = 1e-4 # Learning rate
 
 #---------_Define Model And Loss Function---------
-policy = Chain(Dense(STATE_SIZE,30,relu),Dense(30,50,relu),Dense(50,ACTION_SIZE))
+policy = Chain(Dense(STATE_SIZE,30,relu),Dense(30,50,relu),Dense(50,ACTION_SIZE)) |> gpu
 
 function loss(s,actions,A_t)
-    pi_ = softmax(policy(s))
+    s = s |> gpu
+    actions = actions |> gpu
+    pi_ = softmax(policy(s)) |> gpu
+    A_t = A_t |> gpu
     logpi = log.(mean(pi_ .* actions) .+ 1f-10)
     return mean(-logpi .* A_t)
 end
 
-opt = ADAM(params(policy),η)
+opt = ADAM(η)
+
 
 println("Model Setup")
 
+function discount_and_normalize_returns(experience)
+    returns = []
+    G_t = 0.0
+
+    for (i,(s,a,r,s_)) in enumerate(experience)
+        G_t = G_t*γ + r
+        push!(returns,G_t)
+    end
+
+    μ = mean(returns)
+    σ² = var(returns)
+
+    dis_normalized_ret = [(G_t - μ)/(σ² + 1f-10) for G_t in returns]
+    return dis_normalized_ret
+end
+
 #-------Overriden action Method--------
 function action(pi::CartPolePolicy,reward,state,action)
-    state= Array(state)
+    state= Array(state) |> gpu
     action_probs = softmax(policy(state))
     action = sample(1:ACTION_SIZE,Weights(action_probs)) - 1
     return action
@@ -52,7 +72,7 @@ function episode!(env,pi = RandomPolicy())
 
     experience = []
     for (s,a,r,s_) in ep
-	OpenAIGym.render(env)
+	# OpenAIGym.render(env)
         push!(experience,(s,a,r,s_))
     end
     
@@ -64,35 +84,34 @@ function train(env)
     l = 0.0
     G_t = 0.0
 
+    ep_r = 0.0
+
+    dis_normalized_ret = discount_and_normalize_returns(experience)
+
     for (i,(s,a,r,s_)) in enumerate(reverse(experience))
         state = Array(s)
         act = zeros(ACTION_SIZE,1)
         act[a+1,1] = 1
-         
-        G_t = γ*G_t + r
-    
+        
+        ep_r = ep_r + r
+        G_t = dis_normalized_ret[i]
         l = l + loss(state,act,G_t)   
         Flux.back!(loss(state,act,G_t))
-        opt()
-	# grads = Tracker.gradient(() -> loss(state,act,G_t), params(policy))
-	
-	# for p in params(policy)
-	# 	update!(opt,params(policy),grads[p])
-	# end
+        update!(opt,params(policy),Tracker.gradient(() -> loss(state,act,G_t), params(policy)))
     end 
     l = l./(1.0*length(experience))
-    return G_t,l
+    return ep_r,l,length(experience)
 end
 
 println("Setup Training")
 
-NUM_EPISODES = 1000
+NUM_EPISODES = 5000
 
 for i in 1:NUM_EPISODES
-    println("---")
+    println("---Episode : $i---")
     reset!(env)
-    returns,loss_val = train(env)
-    println("Returns : $returns")
+    returns,loss_val,len = train(env)
+    println("Rewards : $returns")
     println("Loss : $loss_val")
+    println("Episode Length : $len")
 end
-
