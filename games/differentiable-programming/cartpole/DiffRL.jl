3375997c454d79a15521da783d11c1fdb645ecd6
Tejan Karmali <tejank10@gmail.com>
2019-04-19 20:00:41 +0530

    cartpole update, w/ boundserror

--- games/differentiable-programming/cartpole/DiffRL.jl
+++ games/differentiable-programming/cartpole/DiffRL.jl
@@ -1,13 +1,14 @@
-# This implementation of DQN on cartpole is to verify the cartpole.jl env
-using Flux, Gym, Printf
-using Flux.Tracker: track, @grad, data#, gradient
-using Flux.Optimise: Optimiser, _update_params!#, update!
+using Flux, Gym, Printf, Zygote
+using Zygote: @adjoint
+using Flux.Optimise: Optimiser, update!
 using Statistics: mean
-using DataStructures: CircularBuffer
-using CuArrays
+#using CuArrays
+
+import Base.sign
 
 #Load game environment
-env = CartPoleEnv()
+
+env = make("CartPole-v0")
 reset!(env)
 
 #ctx = Ctx(env)
@@ -18,18 +19,17 @@ reset!(env)
 
 # ----------------------------- Parameters -------------------------------------
 
-STATE_SIZE = length(env.state)
-ACTION_SIZE = length(env.action_space)
-MAX_TRAIN_REWARD = env.x_threshold*env.θ_threshold_radians
+STATE_SIZE = length(state(env))
+ACTION_SIZE = 1#length(env_wrap.env.action_space)
+MAX_TRAIN_REWARD = env._env.x_threshold * env._env.θ_threshold_radians
 SEQ_LEN = 8
 
 # Optimiser params
 η = 3f-2
 
 # ------------------------------ Model Architecture ----------------------------
-
-sign(x::TrackedArray) = track(sign, x)
-@grad sign(x) = Base.sign.(data(x)), x̄ -> (x̄,)
+sign(x) = Base.sign.(x)
+@adjoint sign(x) = sign(x), x̄ -> (x̄,)
 
 model = Chain(Dense(STATE_SIZE, 24, relu),
               Dense(24, 48, relu),
@@ -39,104 +39,99 @@ opt = ADAM(η)
 
 action(state) = model(state)
 
-function loss(rewards)
-  ep_len = size(rewards, 1)
-  max_rewards = ones(Float32, ep_len) * MAX_TRAIN_REWARD |> gpu
-  Flux.mse(rewards, max_rewards)
-end
+loss(rewards) = Flux.mse(rewards, MAX_TRAIN_REWARD)
 
 # ----------------------------- Helper Functions -------------------------------
 
-function train_reward()
-  state = env.state
-  x, ẋ, θ, θ̇  = state[1:1], state[2:2], state[3:3], state[4:4]
-  # Custom reward for training
-  # Product of Triangular function over x-axis and θ-axis
-  # Min reward = 0, Max reward = env.x_threshold * env.θ_threshold_radians
-  x_upper = env.x_threshold .- x
-  x_lower = env.x_threshold .+ x
+function train_reward(env::EnvWrapper)
+    s = state(env)
+    x, ẋ, θ, θ̇  = s[1:1], s[2:2], s[3:3], s[4:4]
+    # Custom reward for training
+    # Product of Triangular function over x-axis and θ-axis
+    # Min reward = 0, Max reward = env.x_threshold * env.θ_threshold_radians
+    x_upper = env._env.x_threshold .- x
+    x_lower = env._env.x_threshold .+ x
 
-  r_x     = max.(0f0, min.(x_upper, x_lower))
+    r_x     = max.(0f0, min.(x_upper, x_lower))
 
-  θ_upper = env.θ_threshold_radians .- θ
-  θ_lower = env.θ_threshold_radians .+ θ
+    θ_upper = env._env.θ_threshold_radians .- θ
+    θ_lower = env._env.θ_threshold_radians .+ θ
 
-  r_θ     = max.(0f0, min.(θ_upper, θ_lower))
+    r_θ     = max.(0f0, min.(θ_upper, θ_lower))
 
-  return r_x .* r_θ
+    return r_x .* r_θ
 end
 
 
-function replay(rewards)
-  #grads = gradient(() -> loss(rewards), params(model))
-  #for p in params(model)
-  #  update!(opt, p, grads[p])
-  #end
-  
-  Flux.back!(loss(rewards))
-  _update_params!(opt, params(model))
-end
+function μEpisode(env::EnvWrapper)
+    l = 0
+    for frames ∈ 1:SEQ_LEN
+        #render(env, ctx)
+        #sleep(0.01)
+        a = action(state(env))
+        s′, r, done, _ = step!(env._env, a)
+		## using `env` instead of `env._env` generates error
+		## Uncommenting following generated BoundsError, but works
 
-#@which _update_params!(opt, params(model))
+        #env.steps += 1
+        #env.total_reward += r
+        #env.done = done
+		#if !isnothing(env.max_episode_steps)
+		#	env.done |= env.steps ≥ env.max_episode_steps
+		#end
+        if trainable(env)
+            l += loss(train_reward(env))
+        end
 
-function episode!(env, train=true)
-  done = false
-  total_reward = 0f0
-  rewards = []
-  frames = 1
-  while !done && frames <= 200
-    #render(env, ctx)
-    #sleep(0.01)
-
-    a = action(env.state)
-    s′, r, done, _ = step!(env, a)
-    total_reward += r
-    
-    train && push!(rewards, train_reward())
-    
-    if train && (frames % SEQ_LEN == 0 || done)
-      rewards = vcat(rewards...)
-      replay(rewards)
-      rewards = []
-      env.state = param(env.state.data)
+        game_over(env) && break
     end
-    
-    frames += 1
-  end
-  total_reward
+    return l
+end
+
+function episode!(env::EnvWrapper)
+    reset!(env)
+    while !game_over(env)
+        if trainable(env)
+            grads = gradient(()->μEpisode(env),
+                           params(model))
+            update!(opt, params(model), grads)
+        else
+            μEpisode(env)
+        end
+    end
+    env.total_reward
 end
 
 # -------------------------------- Testing -------------------------------------
 
-function test()
-  score_mean = 0f0
-  for _=1:100
-    reset!(env)
-    total_reward = episode!(env, false)
-    score_mean += total_reward / 100
-  end
-  return score_mean
+function test(env::EnvWrapper)
+    score_mean = 0f0
+    testmode!(env)
+    for _=1:100
+        total_reward = episode!(env)
+        score_mean += total_reward / 100
+    end
+    testmode!(env, false)
+    return score_mean
 end
 
 # ------------------------------ Training --------------------------------------
 
 e = 1
-
 while true
-  global e
-  reset!(env)
-  total_reward = episode!(env)
-  print("Episode: $e | Score: $total_reward | ")
+    global e
+    total_reward = @sprintf "%6.2f" episode!(env)
+    print("Episode: $e | Score: $total_reward | ")
 
-  score_mean = test()
-  score_mean_str = @sprintf "%6.2f" score_mean
-  print("Mean score over 100 test episodes: " * score_mean_str)
+    score_mean = test(env)
+    score_mean_str = @sprintf "%6.2f" score_mean
+    print("Mean score over 100 test episodes: " * score_mean_str)
 
-  println()
+    println()
 
-  if score_mean > 195
-    println("CartPole-v0 solved!")
+    if score_mean > env.reward_threshold
+        println("CartPole-v0 solved!")
     break
-  end
-  e += 1
+    end
+    e += 1
 end
