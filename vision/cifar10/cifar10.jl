eb33eab98d29f6b8a1c5c7e476627d3dbdf5185b
SudhanshuAgrawal27 <sudhanshuagrawal2001@gmail.com>
2019-06-07 14:09:49 +0530

    Changed the optimiser of the cifar10 program from ADAM to Momentum

--- vision/cifar10/cifar10.jl
+++ vision/cifar10/cifar10.jl
@@ -116,7 +116,7 @@ accuracy(x, y) = mean(onecold(m(x), 1:10) .== onecold(y, 1:10))
 
 evalcb = throttle(() -> @show(accuracy(valX, valY)), 10)
 
-opt = ADAM()
+opt = Momentum()
 
 # Starting to train models
 
