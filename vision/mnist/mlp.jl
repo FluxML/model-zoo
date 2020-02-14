344c4dd3cf9c83ddfbd84937785914c89a66a1a7
Lyndon White <oxinabox@ucc.asn.au>
2020-02-14 18:36:54 +0000

    How about we don't use actively terrible julia code as a common demo?

--- vision/mnist/mlp.jl
+++ vision/mnist/mlp.jl
@@ -4,34 +4,32 @@ using Base.Iterators: repeated
 # using CuArrays
 
 # Classify MNIST digits with a simple multi-layer-perceptron
+function demo(
+    imgs = MNIST.images(),
+    m = Chain(Dense(28^2, 32, relu), Dense(32, 10), softmax) |> gpu
+)
+  imgs = MNIST.images()
+  # Stack images into one large batch
+  X = reduce(hcat, float.(reshape.(imgs, :))) |> gpu
 
-imgs = MNIST.images()
-# Stack images into one large batch
-X = hcat(float.(reshape.(imgs, :))...) |> gpu
+  labels = MNIST.labels()
+  # One-hot-encode the labels
+  Y = onehotbatch(labels, 0:9) |> gpu
 
-labels = MNIST.labels()
-# One-hot-encode the labels
-Y = onehotbatch(labels, 0:9) |> gpu
+  dataset = repeated((X, Y), 200)
+  evalcb = () -> @show(loss(X, Y))
+  opt = ADAM()
+  loss(x, y) = crossentropy(m(x), y)
+  accuracy(x, y) = mean(onecold(m(x)) .== onecold(y))
+  Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10))
 
-m = Chain(
-  Dense(28^2, 32, relu),
-  Dense(32, 10),
-  softmax) |> gpu
+  @show accuracy(X, Y)
 
-loss(x, y) = crossentropy(m(x), y)
+  # Test set accuracy
+  tX = hcat(float.(reshape.(MNIST.images(:test), :))...) |> gpu
+  tY = onehotbatch(MNIST.labels(:test), 0:9) |> gpu
 
-accuracy(x, y) = mean(onecold(m(x)) .== onecold(y))
+  @show accuracy(tX, tY)
+end
 
-dataset = repeated((X, Y), 200)
-evalcb = () -> @show(loss(X, Y))
-opt = ADAM()
-
-Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10))
-
-accuracy(X, Y)
-
-# Test set accuracy
-tX = hcat(float.(reshape.(MNIST.images(:test), :))...) |> gpu
-tY = onehotbatch(MNIST.labels(:test), 0:9) |> gpu
-
-accuracy(tX, tY)
+demo()
