f48c7dd9526d0476594205873267995cc9666e9f
matsueushi <matsueushi@gmail.com>
2020-03-01 22:48:01 +0000

    Use Parameters.jl to hold hyperparameters

--- vision/dcgan_mnist/dcgan.jl
+++ vision/dcgan_mnist/dcgan.jl
@@ -5,22 +5,25 @@ using Flux: logitbinarycrossentropy
 using Images
 using MLDatasets
 using Statistics
+using Parameters
 using Printf
 
-const BATCH_SIZE = 128
-const LATENT_DIM = 100
-const EPOCHS = 20
-const VERBOSE_FREQ = 1000
-const OUTPUT_X = 6
-const OUTPUT_Y = 6
-const LR_DSCR = 0.0002
-const LR_GEN = 0.0002
+@with_kw struct HyperParams
+    batch_size::Int = 128
+    latent_dim::Int = 100
+    epochs::Int = 20
+    verbose_freq::Int = 1000
+    output_x::Int = 6
+    output_y::Int = 6
+    lr_dscr::Float64 = 0.0002
+    lr_gen::Float64 = 0.0002
+end
 
-function create_output_image(gen, fixed_noise)
+function create_output_image(gen, fixed_noise, hparams)
     @eval Flux.istraining() = false
     fake_images = @. cpu(gen(fixed_noise))
     @eval Flux.istraining() = true
-    image_array = dropdims(reduce(vcat, reduce.(hcat, partition(fake_images, OUTPUT_Y))); dims=(3, 4))
+    image_array = dropdims(reduce(vcat, reduce.(hcat, partition(fake_images, hparams.output_y))); dims=(3, 4))
     image_array = @. Gray(image_array + 1f0) / 2f0
     return image_array
 end
@@ -34,8 +37,8 @@ end
 
 generator_loss(fake_output) = mean(logitbinarycrossentropy.(fake_output, 1f0))
 
-function train_discriminator!(gen, dscr, batch, opt_dscr)
-    noise = randn(Float32, LATENT_DIM, BATCH_SIZE) |> gpu
+function train_discriminator!(gen, dscr, batch, opt_dscr, hparams)
+    noise = randn(Float32, hparams.latent_dim, hparams.batch_size) |> gpu
     fake_input = gen(noise)
     ps = Flux.params(dscr)
     # Taking gradient
@@ -47,8 +50,8 @@ function train_discriminator!(gen, dscr, batch, opt_dscr)
     return loss
 end
 
-function train_generator!(gen, dscr, batch, opt_gen)
-    noise = randn(Float32, LATENT_DIM, BATCH_SIZE) |> gpu
+function train_generator!(gen, dscr, batch, opt_gen, hparams)
+    noise = randn(Float32, hparams.latent_dim, hparams.batch_size) |> gpu
     ps = Flux.params(gen)
     # Taking gradient
     loss, back = Flux.pullback(ps) do
@@ -60,14 +63,17 @@ function train_generator!(gen, dscr, batch, opt_gen)
 end
 
 function train()
+    # Model Parameters
+    hparams = HyperParams()
+
     # Load MNIST dataset
     images, _ = MLDatasets.MNIST.traindata(Float32)
     # Normalize to [-1, 1] and convert it to WHCN
     image_tensor = permutedims(reshape(@.(2f0 * images - 1f0), 28, 28, 1, :), (2, 1, 3, 4))
     # Partition into batches
-    data = [image_tensor[:, :, :, r] |> gpu for r in partition(1:60000, BATCH_SIZE)]
+    data = [image_tensor[:, :, :, r] |> gpu for r in partition(1:60000, hparams.batch_size)]
 
-    fixed_noise = [randn(LATENT_DIM, 1) |> gpu for _=1:OUTPUT_X*OUTPUT_Y]
+    fixed_noise = [randn(hparams.latent_dim, 1) |> gpu for _=1:hparams.output_x*hparams.output_y]
 
     # Discriminator
     dscr =  Chain(
@@ -82,7 +88,7 @@ function train()
 
     # Generator
     gen = Chain(
-        Dense(LATENT_DIM, 7 * 7 * 256),
+        Dense(hparams.latent_dim, 7 * 7 * 256),
         BatchNorm(7 * 7 * 256, relu),
         x->reshape(x, 7, 7, 256, :),
         ConvTranspose((5, 5), 256 => 128; stride = 1, pad = 2),
@@ -93,30 +99,30 @@ function train()
         ) |> gpu
 
     # Optimizers
-    opt_dscr = ADAM(LR_DSCR)
-    opt_gen = ADAM(LR_GEN)
+    opt_dscr = ADAM(hparams.lr_dscr)
+    opt_gen = ADAM(hparams.lr_gen)
 
     # Training
     train_steps = 0
-    for ep in 1:EPOCHS
+    for ep in 1:hparams.epochs
         @info "Epoch $ep"
         for batch in data
             # Update discriminator and generator
-            loss_dscr = train_discriminator!(gen, dscr, batch, opt_dscr)
-            loss_gen = train_generator!(gen, dscr, batch, opt_gen)
+            loss_dscr = train_discriminator!(gen, dscr, batch, opt_dscr, hparams)
+            loss_gen = train_generator!(gen, dscr, batch, opt_gen, hparams)
 
-            if train_steps % VERBOSE_FREQ == 0
+            if train_steps % hparams.verbose_freq == 0
                 @info("Train step $(train_steps), Discriminator loss = $(loss_dscr), Generator loss = $(loss_gen)")
                 # Save generated fake image
                 # Sample outputs can be found at https://github.com/FluxML/model-zoo/pull/207
-                output_image = create_output_image(gen, fixed_noise)
+                output_image = create_output_image(gen, fixed_noise, hparams)
                 save(@sprintf("dcgan_steps_%06d.png", train_steps), output_image)
             end
             train_steps += 1
         end
     end
 
-    output_image = create_output_image(gen, fixed_noise)
+    output_image = create_output_image(gen, fixed_noise, hparams)
     save(@sprintf("dcgan_steps_%06d.png", train_steps), output_image)
 end
 
