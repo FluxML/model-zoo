72f7e34c782b2d437e23a8e6e50687d8075153ba
Adarshkumar712 <Adarshkumar712.ak@gmail.com>
2020-03-15 04:00:35 +0530

    Updated bitstring-parity, char-rnn.jl, lang-detection/model.jl

--- text/lang-detection/model.jl
+++ text/lang-detection/model.jl
@@ -3,46 +3,73 @@ using Flux: onehot, onehotbatch, crossentropy, reset!, throttle
 using Statistics: mean
 using Random
 using Unicode
+using Parameters: @with_kw
 
-corpora = Dict()
-
-cd(@__DIR__)
-for file in readdir("corpus")
-    lang = Symbol(match(r"(.*)\.txt", file).captures[1])
-    corpus = split(String(read("corpus/$file")), ".")
-    corpus = strip.(Unicode.normalize.(corpus, casefold=true, stripmark=true))
-    corpus = filter(!isempty, corpus)
-    corpora[lang] = corpus
+@with_kw mutable struct Args
+    lr::Float64 = 1e-3     # learning rate
+    N::Int = 15            # Number of perceptrons in hidden layer
+    test_len::Int = 100    # length of test data
+    langs_len::Int = 0     # Number of different languages in Corpora
+    alphabet_len::Int = 0  # Total number of characters possible, in corpora
+    throttle::Int = 10     # throttle timeout
 end
 
-langs = collect(keys(corpora))
-alphabet = ['a':'z'; '0':'9'; ' '; '\n'; '_']
+function get_processed_data(args)
+    corpora = Dict()
 
-# See which chars will be represented as "unknown"
-unique(filter(x -> x ∉ alphabet, join(vcat(values(corpora)...))))
+    for file in readdir("corpus")
+        lang = Symbol(match(r"(.*)\.txt", file).captures[1])
+        corpus = split(String(read("corpus/$file")), ".")
+        corpus = strip.(Unicode.normalize.(corpus, casefold=true, stripmark=true))
+        corpus = filter(!isempty, corpus)
+        corpora[lang] = corpus
+    end
 
-dataset = [(onehotbatch(s, alphabet, '_'), onehot(l, langs))
-           for l in langs for s in corpora[l]] |> shuffle
+    langs = collect(keys(corpora))
+    args.langs_len = length(langs)
+    alphabet = ['a':'z'; '0':'9'; ' '; '\n'; '_']
+    args.alphabet_len = length(alphabet)
 
-train, test = dataset[1:end-100], dataset[end-99:end]
+    # See which chars will be represented as "unknown"
+    unique(filter(x -> x ∉ alphabet, join(vcat(values(corpora)...))))
 
-N = 15
+    dataset = [(onehotbatch(s, alphabet, '_'), onehot(l, langs)) for l in langs for s in corpora[l]] |> shuffle
 
-scanner = Chain(Dense(length(alphabet), N, σ), LSTM(N, N))
-encoder = Dense(N, length(langs))
+    train, test = dataset[1:end-args.test_len], dataset[end-args.test_len+1:end]
+    return train, test
+end
 
-function model(x)
+function Construct_Model(args)
+    scanner = Chain(Dense(args.alphabet_len, args.N, σ), LSTM(args.N, args.N))
+    encoder = Dense(args.N, args.langs_len)
+    return scanner, encoder
+end
+ 
+function model(x, scanner, encoder)
     state = scanner.(x.data)[end]
     reset!(scanner)
     softmax(encoder(state))
 end
 
-loss(x, y) = crossentropy(model(x), y)
+function train(; kws...)
+    # Initialize Hyperparameters
+    args = Args(; kws...)
+    # Load Data
+    train_data, test_data = get_processed_data(args)
 
-testloss() = mean(loss(t...) for t in test)
+    @info("Constructing Model...")
+    scanner, encoder = Construct_Model(args)
 
-opt = ADAM()
-ps = params(scanner, encoder)
-evalcb = () -> @show testloss()
+    loss(x, y) = crossentropy(model(x, scanner, encoder), y)
 
-Flux.train!(loss, ps, train, opt, cb = throttle(evalcb, 10))
+    testloss() = mean(loss(t...) for t in test_data)
+    
+    opt = ADAM(args.lr)
+    ps = params(scanner, encoder)
+    evalcb = () -> @show testloss()
+    @info("Training...")
+    Flux.train!(loss, ps, train_data, opt, cb = throttle(evalcb, args.throttle))
+end
+
+cd(@__DIR__)
+train()
