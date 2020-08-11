5fc22e527f80980529fd27ee70be70a7ee70e6ee
SubhadityMukherjee <msubhaditya@gmail.com>
2020-08-11 17:19:51 +0400

    added VGG models as a first part of restructuring

---
+++ vision/VGG/VGG16.jl
@@ -0,0 +1,76 @@
+using Flux, Statistics
+using Flux: onehotbatch, onecold, crossentropy, throttle
+using Base.Iterators: repeated, partition
+using Metalhead:trainimgs, CIFAR10
+using Images
+
+using Flatten
+
+getarray(X) = Float32.(permutedims(channelview(X), (2, 3, 1)))
+
+X = trainimgs(CIFAR10)
+imgs = [getarray(X[i].img) for i in 1:50000];
+labels = onehotbatch([X[i].ground_truth.class for i in 1:50000],1:10);
+train = gpu.([(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(1:49000, 100)]);
+
+valset = collect(49001:50000)
+valX = cat(imgs[valset]..., dims = 4) |> gpu
+valY = labels[:, valset] |> gpu
+
+vgg16() = Chain(
+  Conv((3, 3), 3 => 64, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(64),
+  Conv((3, 3), 64 => 64, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(64),
+  MaxPool((2,2)),
+  Conv((3, 3), 64 => 128, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(128),
+  Conv((3, 3), 128 => 128, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(128),
+  MaxPool((2,2)),
+  Conv((3, 3), 128 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(256),
+  MaxPool((2,2)),
+  Conv((3, 3), 256 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  MaxPool((2,2)),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
+  BatchNorm(512),
+  MaxPool((2,2)),
+  x -> reshape(x, :, size(x, 4)),
+  Dense(512, 4096, relu),
+  Dropout(0.5),
+  Dense(4096, 4096, relu),
+  Dropout(0.5),
+  Dense(4096, 10),
+  softmax) |> gpu
+
+m = vgg16()
+
+conv_block(in_channels, out_channels) = return Chain(
+    Conv((3,3), in_channels => out_channels, relu, pad = (1,1), stride = (1,1)), 
+    BatchNorm(out_channels))
+
+loss(x, y) = crossentropy(m(x), y)
+
+Chain(double_conv(2, 3))
+
+accuracy(x, y) = mean(onecold(m(x)) .== onecold(y))
+
+evalcb = throttle(() -> @show(accuracy(valX, valY)), 10)
+
+opt = ADAM()
+
+Flux.train!(loss, params(m), train, opt, cb = evalcb)
