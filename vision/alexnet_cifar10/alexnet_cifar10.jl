fb56c8d24b2eb3c56917cd37597931d9632d1f2a
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-03-07 07:08:53 +0100

    Remove tab

--- vision/alexnet_cifar10/alexnet_cifar10.jl
+++ vision/alexnet_cifar10/alexnet_cifar10.jl
@@ -59,21 +59,21 @@ end
 # For more information refer to: https://github.com/FluxML/model-zoo/issues/334
 function set_model(; imgsize=(64, 64,3), num_classes=10)
     return Chain(
-                Conv((11, 11), imgsize[end]=>64, stride=(4,4), relu, pad=(2,2)),
-                MaxPool((3, 3), stride=(2,2)),  
-                Conv((5, 5), 64=>192, relu, pad=(2,2)),
-                MaxPool((3, 3), stride=(2,2)),
-                Conv((3, 3), 192=>384, relu, pad=(1,1)),
-                Conv((3, 3), 384=>256, relu, pad=(1,1)),
-                Conv((3, 3), 256=>256, relu, pad=(1,1)),
-                MaxPool((3, 3), stride=(2,2)),
-                # AdaptiveMeanPool((6, 6)), 
-                flatten,
-                Dropout(0.5),
-                Dense(256*1*1, 4096, relu), # With AdaptiveMeanPool((6, 6)) set 256 * 6 * 6
-                Dropout(0.5),
-                Dense(4096, 4096, relu),
-                Dense(4096, num_classes))
+            Conv((11, 11), imgsize[end]=>64, stride=(4,4), relu, pad=(2,2)),
+            MaxPool((3, 3), stride=(2,2)),  
+            Conv((5, 5), 64=>192, relu, pad=(2,2)),
+            MaxPool((3, 3), stride=(2,2)),
+            Conv((3, 3), 192=>384, relu, pad=(1,1)),
+            Conv((3, 3), 384=>256, relu, pad=(1,1)),
+            Conv((3, 3), 256=>256, relu, pad=(1,1)),
+            MaxPool((3, 3), stride=(2,2)),
+            # AdaptiveMeanPool((6, 6)), 
+            flatten,
+            Dropout(0.5),
+            Dense(256*1*1, 4096, relu), # With AdaptiveMeanPool((6, 6)) set 256 * 6 * 6
+            Dropout(0.5),
+            Dense(4096, 4096, relu),
+            Dense(4096, num_classes))
 end
 
 
