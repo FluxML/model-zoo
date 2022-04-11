4baf62f92f83a1d3252185b7e0ee2c2ef22920d4
Jake Taylor <43712099+jakee417@users.noreply.github.com>
2022-04-10 22:17:45 -0700

    complete train, readme, add comments/asserts

---
+++ vision/diffusion_mnist/README.md
@@ -0,0 +1,27 @@
+# Score-Based Generative Modeling (Diffusion Model)
+![sde](../diffusion_mnist/docs/sde.png)
+[Source](https://yang-song.github.io/blog/2021/score/#score-based-generative-modeling-with-stochastic-differential-equations-sdes)
+
+## Model Info
+TODO
+
+## Training
+TODO
+```shell
+cd vision/diffusion_mnist
+julia --project diffusion_mnist.jl
+```
+
+## Visualization
+TODO
+```shell
+julia --project vae_plot.jl
+```
+
+## References
+
+* [Yang Song. “Generative Modeling by Estimating Gradients of the Data Distribution.” Blog Post, 05May2021](https://yang-song.github.io/blog/2021/score/)
+
+* [Yang Song, Jascha Sohl-Dickstein, Diederik P. Kingma, Abhishek Kumar, Stefano Ermon, and Ben Poole. "Score-Based Generative Modeling Through
+Stochastic Differential Equations". ArXiv Preprint, 10Feb2021](https://arxiv.org/pdf/2011.13456.pdf)
+
--- vision/diffusion_mnist/diffusion_mnist.jl
+++ vision/diffusion_mnist/diffusion_mnist.jl
@@ -7,8 +7,9 @@
 
 using MLDatasets
 using Flux
-using Flux: @functor
+using Flux: @functor, chunk
 using Flux.Data: DataLoader
+using Zygote: ignore
 using Parameters: @with_kw
 using BSON
 using CUDA
@@ -22,68 +23,81 @@ using Random
 # load MNIST images and return loader
 function get_data(batch_size)
     xtrain, ytrain = MLDatasets.MNIST.traindata(Float32)
-    # Normalize to [-1, 1]
-    xtrain = reshape(@.(2.0f0 * xtrain - 1.0f0), 28, 28, 1, :)
+    xtrain = reshape(xtrain, 28, 28, 1, :)
     DataLoader((xtrain, ytrain), batchsize=batch_size, shuffle=true)
 end
 
 """
 Projection of Gaussian Noise onto a time vector.
 
-References:
-    paper-  https://arxiv.org/abs/2006.10739
-    layers- https://fluxml.ai/Flux.jl/stable/models/basics/#Building-Layers
+# Notes
+W is not trainable and is sampled once upon construction.
+
+# References
+paper-  https://arxiv.org/abs/2006.10739 \n
+layers- https://fluxml.ai/Flux.jl/stable/models/basics/#Building-Layers
 """
 function GaussianFourierProjection(embed_dim, scale)
     W = randn(Float32, embed_dim ÷ 2) .* scale
-    function (t)
-        # size(t) => (batch)
-        t_proj = t' .* W * 2π
-        # returns => (embed_dim, batch)
-        return [sin.(t_proj); cos.(t_proj)]
+    function GaussFourierProject(t)
+        t_proj = t' .* W * Float32(2π)
+        [sin.(t_proj); cos.(t_proj)]
     end
 end
+
+gaussfourierproj_test = GaussianFourierProjection(32, 20.0f0)
+# GaussianFourierProjection(embed_dim, ⋅)(batch) => (embed_dim, batch)
+@assert gaussfourierproj_test(randn(Float32, 32)) |> size == (32, 32)
+# W is fixed wrt. repeated calls
+@assert gaussfourierproj_test(
+    ones(Float32, 32)) == 
+    gaussfourierproj_test(ones(Float32, 32)
+)
+# W is not trainable
+@assert params(gaussfourierproj_test) == Flux.Params([])
 
 """
 Create a UNet architecture as a backbone to a diffusion model.
 
-Notes:
-    Images stored in WHCN (width, height, channels, batch) order.
-    In our case, MNIST comes in as (28, 28, 1, batch).
+# Notes
+Images stored in WHCN (width, height, channels, batch) order.
+In our case, MNIST comes in as (28, 28, 1, batch).
 
-References:
-    paper-  https://arxiv.org/abs/1505.04597
-    Conv-   https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.Conv
-    TConv-  https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.ConvTranspose
-    GNorm-  https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.GroupNorm
+# References
+paper-  https://arxiv.org/abs/1505.04597 \n
+Conv-   https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.Conv \n
+TConv-  https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.ConvTranspose \n
+GNorm-  https://fluxml.ai/Flux.jl/stable/models/layers/#Flux.GroupNorm \n
+Flux-   https://fluxml.ai/Flux.jl/stable/models/advanced/#Custom-Model-Example
 """
 struct UNet
     # Embedding
-    embed
+    gaussfourierproj
+    linear::Dense
     # Encoder
-    conv1
-    dense1
-    gnorm1
-    conv2
-    dense2
-    gnorm2
-    conv3
-    dense3
-    gnorm3
-    conv4
-    dense4
-    gnorm4
+    conv1::Conv
+    dense1::Dense
+    gnorm1::GroupNorm
+    conv2::Conv
+    dense2::Dense
+    gnorm2::GroupNorm
+    conv3::Conv
+    dense3::Dense
+    gnorm3::GroupNorm
+    conv4::Conv
+    dense4::Dense
+    gnorm4::GroupNorm
     # Decoder
-    tconv4
-    dense5
-    tgnorm4
-    tconv3
-    dense6
-    tgnorm3
-    tconv2
-    dense7
-    tgnorm2
-    tconv1
+    tconv4::ConvTranspose
+    dense5::Dense
+    tgnorm4::GroupNorm
+    tconv3::ConvTranspose
+    dense6::Dense
+    tgnorm3::GroupNorm
+    tconv2::ConvTranspose
+    dense7::Dense
+    tgnorm2::GroupNorm
+    tconv1::ConvTranspose
     # Scaling Factor
     marginal_prob_std
 end
@@ -94,10 +108,8 @@ User Facing API for UNet architecture.
 function UNet(marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256, scale=30.0f0)
     UNet(
         # Embedding
-        Chain(
-            GaussianFourierProjection(embed_dim, scale),
-            Dense(embed_dim, embed_dim)
-        ),
+        GaussianFourierProjection(embed_dim, scale),
+        Dense(embed_dim, embed_dim),
         # Encoding
         Conv((3, 3), 1 => channels[1], stride=1, bias=false),
         Dense(embed_dim, channels[1]),
@@ -115,21 +127,29 @@ function UNet(marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256, sca
         ConvTranspose((3, 3), channels[4] => channels[3], stride=2, bias=false),
         Dense(embed_dim, channels[3]),
         GroupNorm(channels[3], 32),
-
+        ######################################################################################################
         # FIXME: Julia does not offer a `output_padding` kwarg such as in:
         # https://pytorch.org/docs/stable/generated/torch.nn.ConvTranspose2d.html#convtranspose2d
-        # 
+        #
         # A fix is referenced here: https://github.com/FluxML/Flux.jl/issues/1319
         # and here: https://github.com/FluxML/Flux.jl/issues/829
-        # But padding with `SamePad()` or (0, 1, 0, 1) gives (10, 10, 64, 3) which is incorrect
+        # But padding with `pad=SamePad()` or `pad=(0, 1, 0, 1)` yields (10, 10, 64, 3) 
+        # which is still incorrect.
         # 
+        # Based off relationship 14 of https://arxiv.org/pdf/1603.07285.pdf:
+        # 𝘰' = 𝘴(𝘪' - 1) + 𝘢 + 𝘬 - 2𝘱
+        # Set 𝘰' = 12, 𝘴 = 2, 𝘪' = 5, 𝘬 = 3, 𝘱 = 0, then
+        # 12 = 2(5 - 1) + a + 3 - 2(0) = 11 + a => a = 1
+        #
         # A (incorrect) hack to get the shapes to work is just to pad by (-1, 0, -1, 0):
         # julia> randn(5, 5, 256, 3) |> ConvTranspose((3, 3), 256 => 64, stride=2, pad=(0, -1, 0, -1)) |> size
         # (12, 12, 64, 3)
         #
         # Which is the correct shape, but seems suspicious (negative padding??).
-        # Why is passing a negative padding even allowed? 😕
-        ConvTranspose((3, 3), channels[3] + channels[3] => channels[2], pad=(0, -1, 0, -1), stride=2, bias=false), Dense(embed_dim, channels[2]),
+        # Why is passing a negative padding even allowed in the first place? 😕
+        ######################################################################################################
+        ConvTranspose((3, 3), channels[3] + channels[3] => channels[2], pad=(0, -1, 0, -1), stride=2, bias=false),
+        Dense(embed_dim, channels[2]),
         GroupNorm(channels[2], 32),
         ConvTranspose((3, 3), channels[2] + channels[2] => channels[1], pad=(0, -1, 0, -1), stride=2, bias=false),
         Dense(embed_dim, channels[1]),
@@ -142,67 +162,141 @@ end
 
 @functor UNet
 
+"""
+Helper function that adds `dims` dimensions to the front of a `AbstractVecOrMat`.
+"""
 expand_dims(x::AbstractVecOrMat, dims::Int=2) = reshape(x, (ntuple(i -> 1, dims)..., size(x)...))
+@assert expand_dims(ones(Float32, 32), 3) |> size == (1, 1, 1, 32)
+
+"""
+Helper function that reverses the order of dimensions.
+"""
+reverse_dims(x) = permutedims(x, reverse(ntuple(i -> i, length(size(x)))))
+reverse_test1 = randn(Float32, 32, 32)
+reverse_test2 = randn(Float32, 28, 28, 1, 32, 32)
+reverse_test3 = randn(Float32, 32)
+@assert reverse_dims(reverse_test1) == reverse_test1'
+# Array and Matrix
+@assert (
+    reverse_test2 .+ expand_dims(reverse_test1, 3) ==
+    reverse_dims(reverse_test2) .+ reverse_test1' |> reverse_dims
+)
+# Array and Vector
+@assert (
+    reverse_test2 .+ expand_dims(reverse_test3, 4) ==
+    reverse_dims(reverse_test2) .+ reverse_test3 |> reverse_dims
+)
 
 """
 Forward pass of a UNet architecture.
+
+Notes:
+    ```julia
+    @assert size(a) == (28, 28, 1, 32, 32)
+    @assert size(b) == (32, 32)
+    reverse_dims(a) .+ reverse_dims(b)' |> reverse_dims
+    ```
+    Is used to broadcast an operation from b to a 
+    without the cost of a copy (using expand_dims for example)
+    which is not allowed (mutating vector) by Zygote in 
+    a custom model.
 """
 function (unet::UNet)(x, t)
     # Embedding
-    embed = swish.(unet.embed(t))
+    embed = unet.gaussfourierproj(t)
+    embed = swish.(unet.linear(embed))
     # Encoder
     h1 = unet.conv1(x)
-    h1 .+= expand_dims(unet.dense1(embed))
+    h1 = reverse_dims(h1) .+ unet.dense1(embed)' |> reverse_dims
     h1 = unet.gnorm1(h1)
     h1 = swish.(h1)
     h2 = unet.conv2(h1)
-    h2 .+= expand_dims(unet.dense2(embed))
+    h2 = reverse_dims(h2) .+ unet.dense2(embed)' |> reverse_dims
     h2 = unet.gnorm2(h2)
     h2 = swish.(h2)
     h3 = unet.conv3(h2)
-    h3 .+= expand_dims(unet.dense3(embed))
+    h3 = reverse_dims(h3) .+ unet.dense3(embed)' |> reverse_dims
     h3 = unet.gnorm3(h3)
     h3 = swish.(h3)
     h4 = unet.conv4(h3)
-    h4 .+= expand_dims(unet.dense4(embed))
+    h4 = reverse_dims(h4) .+ unet.dense4(embed)' |> reverse_dims
     h4 = unet.gnorm4(h4)
     h4 = swish.(h4)
     # Decoder
     h = unet.tconv4(h4)
-    h .+= expand_dims(unet.dense5(embed))
+    h = reverse_dims(h) .+ unet.dense5(embed)' |> reverse_dims
     h = unet.tgnorm4(h)
     h = swish.(h)
     h = unet.tconv3(cat(h, h3; dims=3))
-    h .+= expand_dims(unet.dense6(embed))
+    h = reverse_dims(h) .+ unet.dense6(embed)' |> reverse_dims
     h = unet.tgnorm3(h)
     h = swish.(h)
     h = unet.tconv2(cat(h, h2, dims=3))
-    h .+= expand_dims(unet.dense7(embed))
+    h = reverse_dims(h) .+ unet.dense7(embed)' |> reverse_dims
     h = unet.tgnorm2(h)
     h = swish.(h)
     h = unet.tconv1(cat(h, h1, dims=3))
     # Scaling Factor
-    h ./ expand_dims(unet.marginal_prob_std(t), 3)
+    reverse_dims(h) ./ unet.marginal_prob_std(t) |> reverse_dims
 end
 
 function marginal_prob_std(t, sigma=25.0f0)
-    sqrt.((sigma .^ (2t) .- 1.0f0) ./ (2.0f0 * log(sigma)))
+    sqrt.((sigma .^ (2t) .- 1.0f0) ./ 2.0f0 ./ log(sigma))
 end
+
+unet_test = UNet(marginal_prob_std)
+x_test = randn(Float32, (28, 28, 1, 32))
+t_test = rand(Float32, 32)
+score_test = unet_test(x_test, t_test);
+@assert score_test |> size == (28, 28, 1, 32);
+@assert typeof(score_test) == Array{Float32,4}
+# @time [unet_test(x_test, t_test) for i in 1:10];
 
 function diffusion_coeff(t, sigma=25.0f0)
     sigma .^ t
 end
 
-function model_loss(model, x, device, ϵ=1e-5)
+"""
+Model loss following the denoising score matching objectives:
+
+# Notes
+Denoising score matching objective:
+```julia
+min wrt. θ (
+    𝔼 wrt. 𝘵 ∼ 𝒰(0, 𝘛)[
+        λ(𝘵) * 𝔼 wrt. 𝘹(0) ∼ 𝒫₀(𝘹) [
+            𝔼 wrt. 𝘹(t) ∼ 𝒫₀ₜ(𝘹(𝘵)|𝘹(0)) [
+                (||𝘚₀(𝘹(𝘵), 𝘵) - ∇ log [𝒫₀ₜ(𝘹(𝘵) | 𝘹(0))] ||₂)²
+        ]
+    ]
+)
+``` 
+Where 𝒫₀ₜ(𝘹(𝘵) | 𝘹(0)) and λ(𝘵), are available analytically and
+𝘚₀(𝘹(𝘵), 𝘵) is estimated by a U-Net architecture.
+
+# References:
+http://www.iro.umontreal.ca/~vincentp/Publications/smdae_techreport.pdf \n
+https://yang-song.github.io/blog/2019/ssm/
+"""
+function model_loss(model, x, device, ϵ=1.0f-5)
     batch_size = size(x)[end]
-    # (batch) of random times to approximate 𝔼[⋅] wrt. t∼𝒰(0, T)
+    # (batch) of random times to approximate 𝔼[⋅] wrt. 𝘪 ∼ 𝒰(0, 𝘛)
     random_t = rand(Float32, batch_size) .* (1.0f0 - ϵ) .+ ϵ |> device
-    # approximate 𝔼[⋅] wrt. x(0)∼𝒫₀(x)
+    # (batch) of perturbations to approximate 𝔼[⋅] wrt. 𝘹(0) ∼ 𝒫₀(𝘹)
     z = randn(Float32, size(x)) |> device
-    std = model.marginal_prob_std(random_t)
-    perturbed_x = x + z .* expand_dims(std, 3)
+    std = expand_dims(model.marginal_prob_std(random_t), 3)
+    # (batch) of perturbed 𝘹(𝘵)'s to approximate 𝔼 wrt. 𝘹(t) ∼ 𝒫₀ₜ(𝘹(𝘵)|𝘹(0))
+    perturbed_x = x + z .* std
+    # 𝘚₀(𝘹(𝘵), 𝘵)
     score = model(perturbed_x, random_t)
-    sum(sum((score .* expand_dims(std, 3) + z) .^ 2, dims=(1, 2, 3))) / batch_size
+    # mean over batches
+    sum(
+        # L₂ norm over WHC dimensions
+        sum(
+            (score .* std + z) .^ 2,
+            dims=(1, 2, 3)
+        )
+    ) / batch_size
 end
 
 function convert_to_image(x, y_size)
@@ -212,12 +306,11 @@ end
 # arguments for the `train` function 
 @with_kw mutable struct Args
     η = 1e-4                # learning rate
-    batch_size = 32         # batch size
-    sample_size = 10        # sampling size for output 
-    epochs = 50             # number of epochs
+    batch_size = 512        # batch size
+    epochs = 20             # number of epochs
     seed = 0                # random seed
     cuda = false            # use CPU
-    verbose_freq = 10       # logging for every verbose_freq iterations
+    verbose_freq = 1        # logging for every verbose_freq iterations
     tblogger = false        # log training with tensorboard
     save_path = "output"    # results path
 end
@@ -246,7 +339,7 @@ function train(; kws...)
     opt = ADAM(args.η)
 
     # parameters
-    ps = Flux.params(encoder.linear, encoder.μ, encoder.logσ, decoder)
+    ps = Flux.params(unet)
 
     !ispath(args.save_path) && mkpath(args.save_path)
 
@@ -255,14 +348,7 @@ function train(; kws...)
         tblogger = TBLogger(args.save_path, tb_overwrite)
     end
 
-    # fixed input
-    original, _ = first(get_data(args.sample_size^2))
-    original = original |> device
-    image = convert_to_image(original, args.sample_size)
-    image_path = joinpath(args.save_path, "original.png")
-    save(image_path, image)
-
-    # training
+    # Training
     train_steps = 0
     @info "Start Training, total $(args.epochs) epochs"
     for epoch = 1:args.epochs
@@ -284,7 +370,6 @@ function train(; kws...)
                     @info "train" loss = loss
                 end
             end
-
             train_steps += 1
         end
     end
@@ -305,7 +390,6 @@ function train(; kws...)
     # @info "Image saved: $(image_path)"
 end
 
-train()
 if abspath(PROGRAM_FILE) == @__FILE__
     train()
 end
