51e6a091aa9d790d3162cfe8a106ad45aa0beac1
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-02-19 13:34:22 -0500

    explicit gradients

--- tutorials/transfer_learning/transfer_learning.jl
+++ tutorials/transfer_learning/transfer_learning.jl
@@ -29,15 +29,20 @@ const im_size = (224, 224)
 tfm = DataAugmentation.compose(ScaleKeepAspect(im_size), CenterCrop(im_size))
 name_to_idx = Dict{String,Int32}("cats" => 1, "dogs" => 2, "panda" => 3)
 
+const mu = [0.485f0, 0.456f0, 0.406f0]
+const sigma = [0.229f0, 0.224f0, 0.225f0]
+
 function getindex(data::ImageContainer, idx::Int)
     path = data.img[idx]
-    img = Images.load(path)
-    img = apply(tfm, Image(img))
-    img = permutedims(channelview(RGB.(itemdata(img))), (3, 2, 1))
-    img = Float32.(img)
-    name = replace(path, r"(.+)\\(.+)\\(.+_\d+)\.jpg" => s"\2")    
+    _img = Images.load(path)
+    _img = itemdata(apply(tfm, Image(_img)))
+    img = collect(channelview(float32.(RGB.(_img))))
+    img = permutedims(img, (3, 2, 1))
+    # img = permutedims((img .- mu) ./ sigma, (3, 2, 1))
+
+    name = replace(path, r"(.+)\\(.+)\\(.+_\d+)\.jpg" => s"\2")
     y = name_to_idx[name]
-    return img, Flux.onehotbatch(y, 1:3)
+    return img, y
 end
 
 # define DataLoaders
@@ -47,7 +52,7 @@ dtrain = Flux.DataLoader(
     ImageContainer(imgs[1:2700]);
     batchsize,
     collate = true,
-    parallel = true
+    parallel = true,
 )
 device == gpu ? dtrain = Flux.CuIterator(dtrain) : nothing
 
@@ -55,9 +60,13 @@ deval = Flux.DataLoader(
     ImageContainer(imgs[2701:3000]);
     batchsize,
     collate = true,
-    parallel = true
+    parallel = true,
 )
 device == gpu ? deval = Flux.CuIterator(deval) : nothing
+
+# for (x, y) in deval
+#     @info typeof(x)
+# end
 
 # Fine-tune | 🐢 mode
 # Load a pre-trained model: 
@@ -68,27 +77,26 @@ function eval_f(m, deval)
     good = 0
     count = 0
     for (x, y) in deval
-        good += sum(Flux.onecold(m(x)) .== Flux.onecold(y))
-        count += size(y, 2)
+        good += sum(Flux.onecold(m(x)) .== y)
+        count += length(y)
     end
     acc = round(good / count, digits = 4)
     return acc
 end
 
-function train_epoch!(m; ps, opt, dtrain)
+function train_epoch!(model; opt, dtrain)
     for (x, y) in dtrain
-        grads = gradient(ps) do
-            Flux.Losses.logitcrossentropy(m(x), y)
+        grads = gradient(model) do m
+            Flux.Losses.logitcrossentropy(m(x), Flux.onehotbatch(y, 1:3))
         end
-        update!(opt, ps, grads)
+        update!(opt, model, grads[1])
     end
 end
 
-ps = Flux.params(m_tot[2:end]);
-opt = Adam(3e-4)
+opt = Flux.setup(Flux.Optimisers.Adam(1e-5), m_tot);
 
-for iter = 1:8
-    @time train_epoch!(m_tot; ps, opt, dtrain)
+for iter = 1:1
+    @time train_epoch!(m_tot; opt, dtrain)
     metric_train = eval_f(m_tot, dtrain)
     metric_eval = eval_f(m_tot, deval)
     @info "train" metric = metric_train
@@ -104,29 +112,28 @@ function eval_f(m_infer, m_tune, deval)
     good = 0
     count = 0
     for (x, y) in deval
-        good += sum(Flux.onecold(m_tune(m_infer(x))) .== Flux.onecold(y))
-        count += size(y, 2)
+        good += sum(Flux.onecold(m_tune(m_infer(x))) .== y)
+        count += length(y)
     end
     acc = round(good / count, digits = 4)
     return acc
 end
 
-function train_epoch!(m_infer, m_tune; ps, opt, dtrain)
+function train_epoch!(m_infer, m_tune; opt, dtrain)
     for (x, y) in dtrain
         infer = m_infer(x)
-        grads = gradient(ps) do
-            Flux.Losses.logitcrossentropy(m_tune(infer), y)
+        grads = gradient(m_tune) do m
+            Flux.Losses.logitcrossentropy(m(infer), Flux.onehotbatch(y, 1:3))
         end
-        update!(opt, ps, grads)
+        update!(opt, m_tune, grads[1])
     end
 end
 
-ps = Flux.params(m_tune);
-opt = Adam(3e-4)
+opt = Flux.setup(Flux.Optimisers.Adam(5e-4), m_tune);
 
 # training loop
-for iter = 1:8
-    @time train_epoch!(m_infer, m_tune; ps, opt, dtrain)
+for iter = 1:1
+    @time train_epoch!(m_infer, m_tune; opt, dtrain)
     metric_train = eval_f(m_infer, m_tune, dtrain)
     metric_eval = eval_f(m_infer, m_tune, deval)
     @info "train" metric = metric_train
