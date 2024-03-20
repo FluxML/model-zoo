72b98df811add2359ea9822d5c009b892b23d151
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2024-03-20 16:08:35 +0100

    revamp vae_mnist (#407)

--- vision/vae_mnist/vae_plot.jl
+++ vision/vae_mnist/vae_plot.jl
@@ -3,11 +3,14 @@ include("vae_mnist.jl")
 using Plots
 
 function plot_result()
-    BSON.@load "output/model.bson" encoder decoder args
-    args = Args(; args...)
-    device = args.cuda && CUDA.has_cuda() ? gpu : cpu
-    encoder, decoder = encoder |> device, decoder |> device
-    # load MNIST images
+    checkpoint = JLD2.load("output/checkpoint.jld2")
+    encoder_state = checkpoint["encoder"]
+    decoder_state = checkpoint["decoder"]
+    args = Args(; checkpoint["args"]...)
+    encoder = Encoder(args.input_dim, args.latent_dim, args.hidden_dim)
+    decoder = Decoder(args.input_dim, args.latent_dim, args.hidden_dim)
+    Flux.loadmodel!(encoder, encoder_state)
+    Flux.loadmodel!(decoder, decoder_state)
     loader = get_data(args.batch_size)
 
     # clustering in the latent space
@@ -15,7 +18,8 @@ function plot_result()
     plt = scatter(palette=:rainbow)
     for (i, (x, y)) in enumerate(loader)
         i < 20 || break
-        μ, logσ = encoder(x |> device)
+        μ, logσ = encoder(x)
+        @assert size(μ, 1) == 2 # Latent_dim has to be 2 for direct visualization, otherwise use PCA or t-SNE
         scatter!(μ[1, :], μ[2, :], 
             markerstrokewidth=0, markeralpha=0.8,
             aspect_ratio=1,
@@ -27,7 +31,7 @@ function plot_result()
     len = Base.length(z)
     z1 = repeat(z, len)
     z2 = sort(z1)
-    x = zeros(Float32, args.latent_dim, len^2) |> device
+    x = zeros(Float32, args.latent_dim, len^2)
     x[1, :] = z1
     x[2, :] = z2
     samples = decoder(x)
